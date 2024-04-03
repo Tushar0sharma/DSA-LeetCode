416e5de6247b6aa8d3953344c262d07b7651eb7c
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-04-03 07:56:14 +0530

    Time: 210 ms (24.74%) | Memory: 45 MB (13.18%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -3,28 +3,28 @@ class Solution {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(0)){
-                    boolean a=solve(board,word,i,j,board[0].length,board.length,0);
-                    if(a) return true;
+                    boolean b=call(board,word,i,j,board[0].length,board.length,0);
+                    if(b) return true;
                 }
             }
         }
         return false;
     }
-    public boolean solve(char[][]board,String word,int row,int col,int m,int n,int idx){
-        if(idx==word.length()){
+    public boolean call(char[][] board, String word,int i,int j,int m,int n,int idx){
+        if(word.length()==idx){
             return true;
         }
-        if(row<0||col<0||row>=n||col>=m||board[row][col]!=word.charAt(idx)){
+        if(i<0 || j<0 ||i>=n || j>=m || board[i][j]!=word.charAt(idx)){
             return false;
         }
-        board[row][col]='#';
+        board[i][j]='#';
         int[]r={0,0,-1,1};
         int[]c={1,-1,0,0};
-        for(int i=0;i<c.length;i++){
-            boolean ans=solve(board,word,row+r[i],col+c[i],m,n,idx+1);
+        for(int k=0;k<c.length;k++){
+            boolean ans=call(board,word,i+r[k],j+c[k],m,n,idx+1);
             if(ans) return true;
         }
-        board[row][col]=word.charAt(idx);
+        board[i][j]=word.charAt(idx);
         return false;
     }
 }
