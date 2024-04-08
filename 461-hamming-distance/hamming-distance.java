91c2af211417901b4aa4d74088c9762473051724
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-04-09 00:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (63.99%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -1,36 +1,11 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        String s1=call(x);
-        String s2=call(y);
-        if(s1.length()!=s2.length()){
-            if(s1.length()<s2.length()){
-                s1=put(s1,s2.length()-s1.length());
-            }
-            else{
-                s2=put(s2,s1.length()-s2.length());
-            }
+        int res=0;
+        int r=x^y;
+        while(r!=0){
+            res+=r%2;
+            r/=2;
         }
-        System.out.println(s1);
-        System.out.println(s2);
-        int cnt=0;
-        for(int i=0;i<s1.length();i++){
-            if(s1.charAt(i)!=s2.charAt(i)) cnt++;
-        }
-        return cnt;
+        return res;
     }
-    public String put(String s1,int r){
-        for(int i=0;i<r;i++){
-            s1=0+s1;
-        }
-        return s1;
-    }
-    public String call(int n){
-        String b=""; 
-        while(n!=0){
-            int num=n%2;
-            b=num+b;
-            n=n/2;
-        }
-        return b;
-   }
 }
