7fb738be1fa91c9209bce2ea32653855373d2c21
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-04-09 17:44:41 +0530

    Time: 6 ms (51.47%) | Memory: 68.6 MB (27.36%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,44 +10,38 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true; 
-        }
-        ListNode dummy=new ListNode(-1);
-        ListNode temp=head;
-         ListNode t=dummy;
-
-        while(temp!=null){
-            ListNode x=new ListNode(temp.val);
-            dummy.next=x;
-            dummy=dummy.next;
-            temp=temp.next;
-
-        }
-     
-        ListNode rev = reverse(head);
-        ListNode first=t.next;  
-        
-        while (first != null) {
-            if (first.val != rev.val) {
-                return false; 
+        ListNode mid=middle(head);
+        ListNode prev=reverse(mid);
+        ListNode right=prev;
+        ListNode left=head;
+        while(right!=null){
+            if(left.val!=right.val){
+                return false;
             }
-            first = first.next;
-            rev=rev.next;
+            right=right.next;
+            left=left.next;
         }
-
         return true;
     }
-
-    public static ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        while (current != null) {
-            ListNode nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
+    public ListNode reverse(ListNode mid){
+        ListNode prev=null;
+        ListNode curr=mid;
+        ListNode next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
-        return prev; 
+        return prev;
+    }
+    public ListNode middle(ListNode head){
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
 }
