d5bcfad61bdaf138f34bab942ed01eabb615fdc7
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-05-07 09:11:39 +0530

    Time: 5 ms (51.55%) | Memory: 45.7 MB (83.28%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -10,24 +10,28 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
+        head=rev(head);
         int carry=0;
         ListNode dummy=new ListNode (0);
         ListNode t=dummy;
-        head=rev(head);
-        while(head!=null){
-            int n=head.val*2+carry;
-            dummy.next=new ListNode(n%10);
-            carry=n/10;
+        while(head!=null || carry!=0){
+            int k1=0;
+            if(head!=null){
+                k1=head.val;
+            }
+            int pro=k1*2+carry;
+            dummy.next=new ListNode(pro%10);
             dummy=dummy.next;
-            head=head.next;
+            carry=pro/10;
+            if(head!=null){
+                head=head.next;
+            }
         }
-        if(carry!=0){
-            dummy.next=new ListNode(carry);
-        }
-        t=rev(t.next);
+        t=t.next;
+        t=rev(t);
         return t;
     }
-    public ListNode rev(ListNode head ){
+    public ListNode rev(ListNode head){
         ListNode prev=null;
         while(head!=null){
             ListNode next=head.next;
