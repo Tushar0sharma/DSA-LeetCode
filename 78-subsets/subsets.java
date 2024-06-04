385bb5a2c607948b0e21037aded470b422dea7a4
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-04 16:28:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (71.02%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ll=new ArrayList<>();
-        List<Integer> l=new ArrayList<>();
-        print(nums,0,l,ll);
+        List<List<Integer>>ll=new ArrayList<>();
+        List<Integer>l=new ArrayList<>();
+        call(l,ll,0,nums);
         return ll;
     }
-    static void print(int[]nums,int i,List<Integer>l,List<List<Integer>> ll){
+    public void call(List<Integer>l,List<List<Integer>>ll,int i,int []nums){
         if(i==nums.length){
             ll.add(new ArrayList<>(l));
-            return ;
+            return;
         }
-        print(nums,i+1,l,ll);
+        call(l,ll,i+1,nums);
         l.add(nums[i]);
-        print(nums,i+1,l,ll);
+        call(l,ll,i+1,nums);
         l.remove(l.size()-1);
-
     }
 }
