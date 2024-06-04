5c614aa44759082500119a0d5c8cec18404e067f
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-04 08:47:21 +0530

    Time: 9 ms (24.25%) | Memory: 42.1 MB (17.96%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,33 +1,18 @@
 class Solution {
     public int longestPalindrome(String s) {
-        // Map<Character,Integer>mp=new HashMap<>();
-        int []arr=new int[256];
+        Map<Character,Integer>mp=new HashMap<>();
+        int oddcnt=0;
         for(char ch:s.toCharArray()){
-            // mp.put(ch,mp.getOrDefault(ch,0)+1);
-            arr[ch-'0']++;
-            System.out.println(ch-'0');
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            if(mp.get(ch)%2==1){
+                oddcnt++;
+            }
+            else{
+                oddcnt--;
+            }
         }
+        if(oddcnt>1) return s.length()-oddcnt+1;
+        return s.length();
 
-        int one=0;
-        int ans=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]%2==0){
-                ans+=arr[i];
-                arr[i]=0;
-            }
-            else if(arr[i]%2!=0 && arr[i]!=1){
-                ans+=arr[i]-1;
-                arr[i]=1;
-                if(one==0){
-                    ans++;
-                    one=1;
-                }
-            }
-            else if(arr[i]==1 && one==0 ){
-                one=1;
-                ans++;
-            }
-        }
-        return ans;
     }
 }
