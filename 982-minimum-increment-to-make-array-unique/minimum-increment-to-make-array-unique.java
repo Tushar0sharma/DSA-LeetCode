eaa602569b5f1f395567dc297dc2748bf10af524
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-14 07:30:28 +0530

    Time: 37 ms (72.13%) | Memory: 59.8 MB (72.46%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -3,9 +3,10 @@ class Solution {
         Arrays.sort(nums);
         int cnt=0;
         for(int i=1;i<nums.length;i++){
-            while(nums[i-1]>=nums[i]){
-                nums[i]++;
-                cnt++;
+            if(nums[i-1]>=nums[i]){
+                int inc=nums[i-1]-nums[i]+1;
+                cnt+=inc;
+                nums[i]=nums[i-1]+1;
             }
         }
         return cnt;
