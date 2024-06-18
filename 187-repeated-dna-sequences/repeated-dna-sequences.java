cb7e9cf2ce84cf0b06494af45b4db45197de48d4
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-18 08:37:26 +0530

    Time: 28 ms (16.86%) | Memory: 53.6 MB (26.51%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,15 +1,13 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        int i=0;
-        int j=10;
         List<String>l=new ArrayList<>();
         Map<String,Integer>mp=new HashMap<>();
-        while(j<=s.length()){
-            String str=s.substring(i++,j++);
-            mp.put(str,mp.getOrDefault(str,0)+1);
-
-            if( mp.get(str)==2){
-                l.add(str);
+        int j=0;
+        for(int i=10;i<=s.length();i++){
+            String ss=s.substring(j++,i);
+            mp.put(ss,mp.getOrDefault(ss,0)+1);
+            if(mp.get(ss)==2){
+                l.add(ss);
             }
         }
         return l;
