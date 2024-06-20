f4719891d9f78a5797448036096e748d2b27747f
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-20 19:31:38 +0530

    Time: 6 ms (73.93%) | Memory: 40.8 MB (71.70%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,76 +1,36 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        solve(board,0,0);
-            
-        
+        solve(board);
     }
-    public boolean solve(char[][]board,int row,int col){
-        // int row,col;
-        // int []emptycell=emptycell(board,n);
-        // row=emptycell[0];
-        // col=emptycell[1];
-        // if (row == -1 && col == -1) {
-        //     return true;
-        // }
-         if (row == board.length) {
-            return true;
-        }
-        if (col == board[0].length) {
-            return solve(board, row + 1, 0);
-        }
-        if (board[row][col] != '.') {
-            return solve(board, row, col + 1);
-        }
-        for(char num='1';num<='9';num++){
-            if(issafe(board,num,row,col)){
-                board[row][col]=num;
-                if(solve(board,row,col+1))
-                   return true;
-                board[row][col]='.';
+    public boolean solve(char[][]board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(isvalid(i,j,board,c)){
+                            board[i][j]=c;
+
+                            if(solve(board)) return true;
+                            else board[i][j]='.';
+                        }   
+                    }
+                    return false;
+                }
             }
         }
-        return false;
+        return true;
     }
-    // public int[] emptycell(char[][]board,int n){
-    //     int[]cell={-1,-1};
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<n;j++){
-    //             if(board[i][j]=='.'){
-    //                 cell[0]=i;
-    //                 cell[1]=j;
-    //                 return cell;
-    //             }
-    //         }
-    //     }
-    //     return cell;
-    // }
-    public boolean issafe(char[][]board,int num,int row,int col){
-        // for(int i=0;i<board.length;i++){
-        //     if(board[i][col]==num || board[row][i]==num){
-        //         return false;
-        //     }
-        // }
-        // int startrow=row- row%3;
-        // int startcol=col- col%3;
-        // for(int i=0;i<3;i++){
-        //     for(int j=0;j<3;j++){
-        //         if(board[startrow+i][startcol+i]==num){
-        //             return false;
-        //         }
-        //     }
-        // }
-         for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == num) {
-                return false;
-            }
-            if (board[row][i] == num) {
-                return false;
-            }
-             int subgridRow = 3 * (row / 3) + i / 3; 
-            int subgridCol = 3 * (col / 3) + i % 3; 
- 
-            if (board[subgridRow][subgridCol] == num) {
-                return false;
+    public boolean isvalid(int row,int col,char[][]board,char num){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==num) return false;
+            if(board[i][col]==num) return false;
+        }
+        int start=row-row%3;
+        int end=col-col%3;
+
+        for(int i=start;i<start+3;i++){
+            for(int j=end;j<end+3;j++){
+                if(board[i][j]==num) return false;
             }
         }
         return true;
