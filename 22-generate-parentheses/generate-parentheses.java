ae7a3c31a75d5264801323545d563f0ddd1e8a4f
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-20 18:41:43 +0530

    Time: 2 ms (53.10%) | Memory: 43.3 MB (33.44%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String>l=new ArrayList<>();
-        call(0,0,"",l,n);
+        call(0,0,n,l,"");
         return l;
     }
-    public void call(int o,int c,String ans,List<String>l,int n){
-        if(o==n && c==n){
+    public void call(int o,int c,int n,List<String>l,String ans){
+        if(o==c && o==n){
             l.add(ans);
-            return;
+            return ;
         }
         if(o<n){
-            call(o+1,c,ans+'(',l,n);
+            call(o+1,c,n,l,ans+"(");
         }
         if(c<o){
-            call(o,c+1,ans+')',l,n);
+            call(o,c+1,n,l,ans+")");
         }
+        return;
     }
 }
