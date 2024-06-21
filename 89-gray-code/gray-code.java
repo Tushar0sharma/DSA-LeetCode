225968b05a597a457e0a25baf6177ca99617008c
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-21 18:04:46 +0530

    Time: 13 ms (24.46%) | Memory: 62.7 MB (14.23%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        boolean[] used=new boolean[1<<n];
         List<Integer>l=new ArrayList<>();
+        boolean []used=new boolean[1<<n];
         l.add(0);
         used[0]=true;
-        backtrack(used,n,l);
+        call(l,used,n);
         return l;
     }
-    public boolean backtrack(boolean[]used,int n,List<Integer>l){
+    public boolean call(List<Integer>l,boolean[]used,int n){
         if(l.size()==1<<n){
             return true;
         }
-
         for(int i=0;i<n;i++){
-            int next=(1<<i) ^ l.get(l.size()-1);
+            int next=l.get(l.size()-1)^(1<<i);
             if(used[next]) continue;
             used[next]=true;
             l.add(next);
-            if(backtrack(used,n,l)) return true;
+            if(call(l,used,n)) return true;
             used[next]=false;
             l.remove(l.size()-1);
         }
