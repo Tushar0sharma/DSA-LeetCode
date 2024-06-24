e7f339ebad78208de93087cc5e78eb91924a04ee
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-24 15:08:58 +0530

    Time: 222 ms (18.31%) | Memory: 44.8 MB (20.50%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,31 +1,26 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(word.charAt(0)==board[i][j]){
-                    boolean a=solve(i,j,board,word,0);
-                    if(a) return true;
+        int n=board.length;
+        int m=board[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word.charAt(0)){
+                    if(call(board,n,m,i,j,word,0)) return true;
                 }
             }
         }
         return false;
     }
-    public boolean solve(int i,int j,char[][]board,String word,int idx){
-        if(idx==word.length()){
-            return true;
-        }
-        if(i>=board.length || j>=board[0].length || i<0 || j<0 || board[i][j]!=word.charAt(idx)){
-            return false;
-        }
-        board[i][j]='#';
+    public boolean call(char[][]board,int n,int m,int i,int j,String w,int idx){
+        if(idx==w.length()) return true;
+        if(i>=n || j>=m || i<0 || j<0 || board[i][j]!=w.charAt(idx) ) return false;
         int []r={1,-1,0,0};
-        int []c={0,0,1,-1};
-
+        int []c={0,0,-1,1};
+        board[i][j]='#';
         for(int k=0;k<4;k++){
-            boolean ans=solve(i+r[k],j+c[k],board,word,idx+1);
-            if(ans) return true;
+            if(call(board,n,m,i+r[k],j+c[k],w,idx+1)) return true;
         }
-        board[i][j]=word.charAt(idx);
+        board[i][j]=w.charAt(idx);
         return false;
     }
 }
