2f1794720225e8b3d09c083e584b57a2bdb5a981
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-26 22:00:08 +0530

    Time: 5 ms (97.87%) | Memory: 51.4 MB (90.43%) - LeetSync

--- 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.java
+++ 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.java
@@ -1,31 +1,9 @@
 class Solution {
     public List<Integer> circularPermutation(int n, int start) {
-        boolean []used=new boolean[1<<n];
-        used[0]=true;
         List<Integer>l=new ArrayList<>();
-        l.add(0);
-        call(l,used,n);
-        List<Integer>res=new ArrayList<>();
-        int idx=l.indexOf(start);
-        for(int i=0;i<l.size();i++){
-            res.add(l.get((idx+i)%l.size()));
+        for(int i=0;i<(1<<n);i++){
+            l.add(start^(i^(i>>1)));
         }
-        return res;
-    }
-    public boolean call(List<Integer>l,boolean[]used,int n){
-        if(l.size()==1<<n){
-            return true;
-        }
-        for(int i=0;i<n;i++){
-            int next=l.get(l.size()-1)^(1<<i);
-            if(!used[next]){
-                used[next]=true;
-                l.add(next);
-                if(call(l,used,n)) return true;
-                used[next]=false;
-                l.remove(l.size()-1);
-            }
-        }
-        return false;
+        return l;
     }
 }
