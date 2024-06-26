b34311aae25b509848f5c86f73367a2843442402
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-26 10:33:55 +0530

    Time: 22 ms (78.99%) | Memory: 56.2 MB (43.84%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int[]a=f(nums,0,nums.length-1);
-        return a;
+        //merge sort algo
+        return call(nums,0,nums.length-1);
     }
-    public int[] f(int []nums,int s,int e){
+    public int[] call(int []nums,int s,int e){
         if(s==e){
-            int[]a=new int[1];
+            int []a=new int[1];
             a[0]=nums[s];
             return a;
         }
         int mid=(s+e)/2;
-        int[] arr1=f(nums,s,mid);
-        int[] arr2=f(nums,mid+1,e);
+        int []arr1=call(nums,s,mid);
+        int []arr2=call(nums,mid+1,e);
         return merge(arr1,arr2);
     }
-    public int[] merge(int[] arr1,int[] arr2){
+    public int[] merge(int []arr1,int []arr2){
+        int []ans=new int[arr1.length+arr2.length];
         int i=0,j=0,k=0;
-        int[]ans=new int[arr1.length+arr2.length];
-        while(i<arr1.length&&j<arr2.length){
-            if(arr1[i]<arr2[j]){
-               ans[k++]=arr1[i++];
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<=arr2[j]){
+                ans[k++]=arr1[i++];
             }
             else{
                 ans[k++]=arr2[j++];
