44c1a8682bc7159dde5a51fbdf1641b872eff4e1
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-28 07:53:46 +0530

    Time: 4 ms (98.64%) | Memory: 51.1 MB (78.85%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,25 +1,10 @@
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer>l=new ArrayList<>();
-        boolean []used=new boolean[1<<n];
-        l.add(0);
-        used[0]=true;
-        call(l,used,n);
+        for(int i=0;i<1<<n;i++){
+            int a=i^i>>1;
+            l.add(a);
+        }
         return l;
-    }
-    public boolean call(List<Integer>l,boolean[]used,int n){
-        if(l.size()==1<<n){
-            return true;
-        }
-        for(int i=0;i<n;i++){
-            int next=l.get(l.size()-1)^(1<<i);
-            if(used[next]) continue;
-            used[next]=true;
-            l.add(next);
-            if(call(l,used,n)) return true;
-            used[next]=false;
-            l.remove(l.size()-1);
-        }
-        return false;
     }
 }
