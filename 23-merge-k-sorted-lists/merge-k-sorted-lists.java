4d763a99e548b3a78f0e7dd176b470a76a0b5902
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-06-29 10:42:49 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (6.51%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -11,19 +11,19 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length==0 || lists==null){
-            return  null;
+            return null;
         }
-        ListNode head=partition(lists,0,lists.length-1);
+        ListNode head=sort(lists,0,lists.length-1);
         return head;
     }
-    public ListNode partition(ListNode[]list,int start,int end){
-        if(start==end){
-            return list[start];
+    public ListNode sort(ListNode []lists,int s,int e){
+        if(s==e){
+            return lists[s];
         }
-        if(start<end){
-            int mid=start+(end-start)/2;
-            ListNode l1=partition(list,start,mid);
-            ListNode l2=partition(list,mid+1,end);
+        if(s<e){
+            int m=(s+e)/2;
+            ListNode l1=sort(lists,s,m);
+            ListNode l2=sort(lists,m+1,e);
             return merge(l1,l2);
         }
         else{
@@ -32,25 +32,19 @@ class Solution {
     }
     public ListNode merge(ListNode l1,ListNode l2){
         ListNode dummy=new ListNode(0);
-        ListNode head=dummy;
+        ListNode h=dummy;
         while(l1!=null && l2!=null){
             if(l1.val<l2.val){
-                dummy.next=l1;
-                dummy=dummy.next;
+                dummy.next=new ListNode(l1.val);
                 l1=l1.next;
             }
             else{
-                dummy.next=l2;
-                dummy=dummy.next;
+                dummy.next=new ListNode(l2.val);
                 l2=l2.next;
             }
+            dummy=dummy.next;
         }
-        if(l1!=null){
-            dummy.next=l1;
-        }
-        if(l2!=null){
-            dummy.next=l2;
-        }
-        return head.next;
+        dummy.next=l1==null?l2:l1;
+        return h.next;
     }
 }
