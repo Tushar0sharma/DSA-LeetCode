1d6092c070478eb3adb86866576999428ae1de15
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-01 23:12:40 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (75.23%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -15,16 +15,15 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        int l=h(root.left);
-        int r=h(root.right);
-        if(Math.abs(l-r)<=1 && isBalanced(root.left) && isBalanced(root.right)){
-            return true;
-        }
-        return false;
+        return dfs(root)!=-1;
     }
-    public int h(TreeNode root){
+    public int dfs(TreeNode root){
         if(root==null) return 0;
-        return 1+Math.max(h(root.left),h(root.right));
+        int l=dfs(root.left);
+        if(l==-1) return -1;
+        int r=dfs(root.right);
+        if(r==-1) return -1;
+        if(Math.abs(l-r)>1) return -1;
+        return 1+Math.max(l,r);
     }
 }
