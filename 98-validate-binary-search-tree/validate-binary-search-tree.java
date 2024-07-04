0a0c94e731303e512808f94ce42c401476082236
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-04 15:24:18 +0530

    Time: 4 ms (16.23%) | Memory: 43 MB (74.41%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -16,29 +16,22 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         if(root==null ) return true;
-        // if(call(root.left)>=root.val || call1(root.right)<=root.val){
-        //     System.out.println(call(root.left));
-        //     System.out.println(call1(root.right));
-        //     System.out.println(root.val);
-        //     return false;
-        // }
-        if (root.left != null && call(root.left) >= root.val) return false;
-        
-        if (root.right != null && call1(root.right) <= root.val) return false;
+        if (root.left != null && max(root.left) >= root.val) return false;
+        if (root.right != null && min(root.right) <= root.val) return false;
         return isValidBST(root.left) && isValidBST(root.right);
     }
-    public int call(TreeNode root){
+    public int max(TreeNode root){
         if(root==null) return Integer.MIN_VALUE;
-        int l=Math.max(call(root.left),root.val);
-        int r=Math.max(call(root.right),root.val);
-        return Math.max(l,r);
+        // int l=Math.max(max(root.left),root.val);
+        // int r=Math.max(max(root.right),root.val);
+        int l=max(root.left);
+        int r=max(root.right);
+        return Math.max(root.val,Math.max(l,r));
     }
-     public int call1(TreeNode root){
-    if (root == null) return Integer.MAX_VALUE; 
-
-    int leftMin = call1(root.left);  
-    int rightMin = call1(root.right); 
-
-    return Math.min(root.val, Math.min(leftMin, rightMin)); 
+    public int min(TreeNode root){
+        if (root == null) return Integer.MAX_VALUE; 
+        int leftMin = min(root.left);  
+        int rightMin = min(root.right); 
+        return Math.min(root.val, Math.min(leftMin, rightMin)); 
     }
 }
