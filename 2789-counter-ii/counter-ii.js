9081fee277ef82f6c07faf54bb1326e473cbb0ea
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-06 14:41:32 +0530

    Time: 57 ms (69.29%) | Memory: 51.7 MB (54.10%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val=init;
+    return{
+        increment:()=>{
+        val++;
+        return val;
+    },
+    decrement:()=>{
+        val--;
+        return val;
+    },
+    reset:()=>{
+        return val=init;
+    }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
