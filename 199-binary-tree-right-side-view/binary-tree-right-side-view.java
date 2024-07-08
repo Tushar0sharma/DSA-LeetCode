b3a54d7c7be352366461c996a292132143050bac
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-08 23:38:58 +0530

    Time: 3 ms (7.00%) | Memory: 42 MB (32.40%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,16 +13,38 @@
  *     }
  * }
  */
+ class pair{
+    TreeNode n;
+    int v;
+    pair(TreeNode n,int v){
+        this.n=n;
+        this.v=v;
+    } 
+    public TreeNode getKey(){
+        return n;
+    }
+    public int getValue(){
+        return v;
+    }
+ }
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer>l=new ArrayList<>();
-        call(root,l,0);        
+        if(root==null) return l;
+        Queue<pair>q=new LinkedList<>();
+        Map<Integer,Integer>mp=new TreeMap<>();
+        q.add(new pair(root,0));
+        while(!q.isEmpty()){
+            pair p=q.poll();
+            int v=p.getValue();
+            TreeNode n=p.getKey();
+            if(!mp.containsKey(v)) mp.put(v,n.val);
+            if(n.right!=null) q.add(new pair(n.right,v+1));
+            if(n.left!=null) q.add(new pair(n.left,v+1));
+        }
+        for(int i:mp.values()){
+            l.add(i);
+        }
         return l;
-    }
-    public void call(TreeNode root,List<Integer>l,int i){
-        if(root==null) return;
-        if(i==l.size()) l.add(root.val);
-        call(root.right,l,i+1);
-        call(root.left,l,i+1);
     }
 }
