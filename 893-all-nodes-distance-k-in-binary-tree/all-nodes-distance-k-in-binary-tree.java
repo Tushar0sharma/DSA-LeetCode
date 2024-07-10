2f3d717fb0f14892ac3f7353f7b7051f01a968fc
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-10 13:30:21 +0530

    Time: 12 ms (89.86%) | Memory: 42.2 MB (81.34%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,47 +8,40 @@
  * }
  */
 class Solution {
+    Map<TreeNode,Integer>mp=new HashMap<>();
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         List<Integer>l=new ArrayList<>();
-        Map<Integer,TreeNode>mp=new HashMap<>();
-        Queue<TreeNode>q=new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int s=q.size();
-            for(int i=0;i<s;i++){
-                TreeNode top=q.poll();
-                if(top.left!=null){
-                    mp.put(top.left.val,top);
-                    q.add(top.left);
-                }
-                if(top.right!=null){
-                    mp.put(top.right.val,top);
-                    q.add(top.right);
-                }
-            }
-        }
-        Map<Integer,Integer>m=new HashMap<>();
-        q.add(target);
-        while(k>0 && !q.isEmpty()){
-            int s=q.size();
-            for(int i=0;i<s;i++){
-                TreeNode top=q.poll();
-                m.put(top.val,1); 
-                if(top.left!=null && !m.containsKey(top.left.val)){
-                    q.add(top.left);
-                }
-                if(top.right!=null && !m.containsKey(top.right.val)){
-                    q.add(top.right);
-                }
-                if(mp.containsKey(top.val) && !m.containsKey(mp.get(top.val).val)){
-                    q.offer(mp.get(top.val));
-                }
-            }
-            k--;
-        }
-        while(!q.isEmpty()){
-            l.add(q.poll().val);
-        }
+        find(root,target);
+        dfs(root,k,mp.get(root),l);
         return l;
+    }
+    public int find(TreeNode root,TreeNode t){
+        if(root==null) return -1;
+        if(root==t){
+            mp.put(root,0);
+            return 0;
+        }
+        int l=find(root.left,t);
+        if(l>=0){
+            mp.put(root,l+1);
+            return l+1;
+        }
+        int r=find(root.right,t);
+        if(r>=0){
+            mp.put(root,r+1);
+            return r+1;
+        }
+        return -1;
+    }
+    public void dfs(TreeNode root,int k,int length,List<Integer>l){
+        if(root==null) return ;
+        if(mp.containsKey(root)){
+            length=mp.get(root);
+        }
+        if(k==length){
+            l.add(root.val);
+        }
+        dfs(root.left,k,length+1,l);
+        dfs(root.right,k,length+1,l);
     }
 }
