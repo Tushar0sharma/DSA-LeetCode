305d56bb259556a760fbd3ef7746f6b7d58b4cc3
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-12 15:25:54 +0530

    Time: 2 ms (71.86%) | Memory: 44.1 MB (68.28%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -19,18 +19,17 @@ class Solution {
         for(int i=0;i<inorder.length;i++){
             mp.put(inorder[i],i);
         }
-        TreeNode root=build(preorder,0,preorder.length-1,inorder,0,inorder.length-1,mp);
+        TreeNode root=build(mp,preorder,0,preorder.length-1,inorder,0,inorder.length-1);
         return root;
     }
-    public TreeNode build(int []pre,int ps,int pe,int[] in,int is,int ie,Map<Integer,Integer>mp){
-        if(ps>pe && is>ie){
-            return null;
-        }
-        TreeNode root=new TreeNode (pre[ps]);
+    public TreeNode build(Map<Integer,Integer>mp,int []preorder,int prest,int preen,int []inorder,int inst,int inen){
+        if(prest>preen || inst>inen) return null;
+        TreeNode root=new TreeNode(preorder[prest]);
         int inroot=mp.get(root.val);
-        int left=inroot-is;
-        root.left=build(pre,ps+1,ps+left,in,is,inroot-1,mp);
-        root.right=build(pre,ps+left+1,pe,in,inroot+1,ie,mp);
-        return root;
+        int numsl=inroot-inst;
+
+        root.left=build(mp,preorder,prest+1,prest+numsl,inorder,inst,inroot-1);
+        root.right=build(mp,preorder,prest+numsl+1,preen,inorder,inroot+1,inen);
+        return root; 
     }
 }
