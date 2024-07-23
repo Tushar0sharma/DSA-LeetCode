813d8791e94de9b74e48e462c40e4df4588ae960
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-23 22:07:43 +0530

    Time: 23 ms (5.36%) | Memory: 55.8 MB (14.02%) - LeetSync

--- 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.java
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.java
@@ -1,33 +1,52 @@
 class Solution {
     public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
-	List<Integer> order1 = GenerateTopologicalSort(rowConditions, k);
-	List<Integer> order2 = GenerateTopologicalSort(colConditions, k);
-	if (order1.size() < k || order2.size() < k) return new int[0][0];
-	Map<Integer, Integer> m = new HashMap();
-	for (int i = 0; i < k; i++) m.put(order2.get(i), i);
-	int[][] ans = new int[k][k];
-	for (int i = 0; i < k; i++)
-		ans[i][m.get(order1.get(i))] = order1.get(i);
-	return ans;
-}
-private List<Integer> GenerateTopologicalSort(int[][] A, int k) {
-	int[] deg = new int[k];
-	List<Integer> order = new ArrayList();
-	List<List<Integer>> graph = new ArrayList();
-	for (int i = 0; i < k; i++) graph.add(new ArrayList());
-	Queue<Integer> q = new LinkedList();
-	for (int[] c: A) {
-		graph.get(c[0] - 1).add(c[1] - 1);
-		deg[c[1] - 1]++;
-	}
-	for (int i = 0; i < k; i++)
-		if (deg[i] == 0) q.add(i);
-	while(!q.isEmpty()) {
-		int x = q.poll();
-		order.add(x + 1);
-		for (int y: graph.get(x)) 
-			if (--deg[y] == 0) q.add(y);
-	}
-	return order;
-}
+        List<Integer>l1=topo(k,rowConditions);
+        List<Integer>l2=topo(k,colConditions);
+        System.out.println(l1);
+        System.out.println(l2);
+        if(l1.isEmpty() || l2.isEmpty()) return new int[0][0];
+        
+        int [][]ans=new int[k][k];
+        for(int i=0;i<k;i++){
+            for(int j=0;j<k;j++){
+                if(l1.get(i).equals(l2.get(j))){
+                    ans[i][j]=l1.get(i);
+                }
+            }
+        }
+        return ans;        
+    }
+    public List<Integer> topo(int n,int [][]row){
+        List<Integer>order=new ArrayList<>();
+        List<List<Integer>>ll=new ArrayList<>();
+        for(int i=0;i<=n;i++) ll.add(new ArrayList<>());
+        for(int []r:row){
+            ll.get(r[0]).add(r[1]);
+        }
+        boolean []cycle={false};
+        int []vis=new int[n+1];
+        for(int i=1;i<=n;i++){
+            if(vis[i]==0){
+                dfs(vis,cycle,order,ll,i);
+                if(cycle[0]) return new ArrayList<>();
+            }
+        }
+        Collections.reverse(order);
+        return order;
+    }
+    public void dfs(int[] vis,boolean[]cycle,List<Integer>order,List<List<Integer>>ll,int i){
+        vis[i]=1;
+        for(int k:ll.get(i)){
+            if(vis[k]==0){
+                dfs(vis,cycle,order,ll,k);
+                if(cycle[0]) return;
+            }
+            else if(vis[k]==1){
+                cycle[0]=true;
+                return;
+            }
+        }
+        vis[i]=2;
+        order.add(i);
+    }
 }
