34f298d1d5568c534803528f773953b60d5ac99c
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-30 16:30:57 +0530

    Time: 7 ms (96.60%) | Memory: 44.1 MB (75.84%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,63 +1,34 @@
-class pair{
-    int source;
-    int w;
-    pair(int source,int w){
-        this.source=source;
-        this.w=w;
-    }
-}
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        List<List<int[]>>ll=new ArrayList<>();
-        for(int i=0;i<n;i++) ll.add(new ArrayList<>());
-        for(int []i:edges){
-            ll.get(i[0]).add(new int[]{i[1],i[2]});
-            ll.get(i[1]).add(new int[]{i[0],i[2]});
+        int[][]mat=new int[n][n];
+        for(int i=0;i<n;i++) Arrays.fill(mat[i],10001);
+        for(int i=0;i<edges.length;i++){
+            mat[edges[i][0]][edges[i][1]]=edges[i][2];
+            mat[edges[i][1]][edges[i][0]]=edges[i][2];
         }
-        int[][]dist=new int[n][n];
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j) dist[i][j]=0;
-                else dist[i][j]=Integer.MAX_VALUE;
+            for(int j=0;j<n;j++) if(i==j) mat[i][j]=0;
+        }
+
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    mat[i][j]=Math.min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
             }
         }
+        int small=n;
+        int city=0;
         for(int i=0;i<n;i++){
-            bfs(i,ll,dist[i],n);
-        }
-        for(int []i:dist) for(int j:i) System.out.print(j+" ");
-        return getcity(distanceThreshold,n,dist);
-        // return 0;
-    }
-    public int getcity(int k,int n,int[][]dist){
-        int cnt=n;
-        int city=-1;
-        for(int i=0;i<n;i++){
-            int cnt1=0;
+            int cnt=0;
             for(int j=0;j<n;j++){
-                if(i==j) continue;
-                else if(dist[i][j]<=k) cnt1++;
+                if(mat[i][j]<=distanceThreshold) cnt++;
             }
-            if(cnt1<=cnt){
-                cnt=cnt1;
+            if(cnt<=small){
+                small=cnt;
                 city=i;
             }
         }
         return city;
     }
-    public void bfs(int src,List<List<int[]>>ll,int[]dist,int n){
-        PriorityQueue<pair>pq=new PriorityQueue<>((a,b)->a.w-b.w);
-        pq.add(new pair(src,0));
-        while(!pq.isEmpty()){
-            pair p=pq.poll();
-            int node=p.source;
-            int weight=p.w;
-            for(int []i:ll.get(node)){
-                if(weight+i[1]<dist[i[0]]){
-                    dist[i[0]]=weight+i[1];
-                    pq.add(new pair(i[0],dist[i[0]]));
-                }
-            }
-        }
-    }
-
 }
