dee048350da3cf1325fe2fa6f70ee6351d26128c
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-06 19:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (41.21%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,26 +1,27 @@
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int [][]dp=new int[m][n];
+//         dp[0][0]=1;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i-1>=0) dp[i][j]=dp[i-1][j];
+//                 if(j-1>=0) dp[i][j]+=dp[i][j-1];
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// }
 class Solution {
     public int uniquePaths(int m, int n) {
-        int [][]dp=new int[m][n];
-        // for(int []i:dp) Arrays.fill(i,-1);
-        // return f(dp,m-1,n-1);
-        dp[0][0]=1;
+        int []dp=new int[n];
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i-1>=0) dp[i][j]=dp[i-1][j];
-                if(j-1>=0) dp[i][j]+=dp[i][j-1];
+            int []temp=new int[n];
+            temp[0]=1;
+            for(int j=1;j<n;j++){
+                temp[j]=temp[j-1]+dp[j];
             }
+            dp=temp;
         }
-        return dp[m-1][n-1];
-    }
-
-    public int f(int [][]dp,int m,int n){
-        if(m==0 && n==0){
-            return dp[m][n]=1;
-        }
-        if(n<0 || m<0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        dp[m][n]=f(dp,m-1,n);
-        dp[m][n]+=f(dp,m,n-1);
-        return dp[m][n];
+        return dp[n-1];
     }
 }
