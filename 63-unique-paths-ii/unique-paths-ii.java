b3b411a39470f9b73768f5876790e4783c38dd81
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-06 21:32:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (75.14%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,17 +3,19 @@ class Solution {
         if(o[o.length-1][o[0].length-1]==1 || o[0][0]==1) return 0;
         int n=o.length;
         int m=o[0].length;
-        int [][]dp=new int[o.length][o[0].length];
-        dp[0][0]=1;
+        int []dp=new int[m];
+        dp[0]=1;
         for(int i=0;i<n;i++){
+            int []temp=new int[m];
+            // temp[0]=1;
             for(int j=0;j<m;j++){
-                if(o[i][j]==1) dp[i][j]=0;
-                else{
-                    if(i-1>=0) dp[i][j]=dp[i-1][j];
-                    if(j-1>=0) dp[i][j]+=dp[i][j-1];
-                }
+                if(j>0)
+                temp[j]=temp[j-1]+dp[j];
+                if(o[i][j]==1) temp[j]=0;
+                else if(j == 0 && dp[j] == 1) temp[j]=1;
             }
+            dp=temp;
         }
-        return dp[n-1][m-1];
+        return dp[m-1];
     }
 }
