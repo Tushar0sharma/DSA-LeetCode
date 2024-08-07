ac11b80e0843bbbc2f59ef7de5828a2c64074069
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-07 14:35:55 +0530

    Time: 3 ms (62.39%) | Memory: 45.4 MB (43.56%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -16,19 +16,39 @@
 //         return dp[n][m];
 //     }
 // }
+// class Solution {
+//     public int minPathSum(int[][] grid) {
+//         int n=grid.length;
+//         int m=grid[0].length;
+//         int [][]dp=new int[n][m];
+//         for(int i[]:dp) Arrays.fill(i,Integer.MAX_VALUE);
+//         dp[0][0]=grid[0][0];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(i-1>=0) dp[i][j]=Math.min(dp[i][j],dp[i-1][j]+grid[i][j]);
+//                 if(j-1>=0) dp[i][j]=Math.min(dp[i][j],dp[i][j-1]+grid[i][j]);
+//             }
+//         }
+//         return dp[n-1][m-1];
+//     }
+// }
 class Solution {
     public int minPathSum(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int [][]dp=new int[n][m];
-        for(int i[]:dp) Arrays.fill(i,Integer.MAX_VALUE);
-        dp[0][0]=grid[0][0];
+        int []dp=new int[m];
         for(int i=0;i<n;i++){
+            int []temp=new int[m];
             for(int j=0;j<m;j++){
-                if(i-1>=0) dp[i][j]=Math.min(dp[i][j],dp[i-1][j]+grid[i][j]);
-                if(j-1>=0) dp[i][j]=Math.min(dp[i][j],dp[i][j-1]+grid[i][j]);
+                if(i==0 && j==0) temp[j]=grid[0][0];
+                else{
+                    int up = (i > 0) ? dp[j] + grid[i][j] : Integer.MAX_VALUE;
+                    int left = (j > 0) ? temp[j - 1] + grid[i][j] : Integer.MAX_VALUE;
+                    temp[j] = Math.min(up, left);
+                }
             }
+            dp=temp;
         }
-        return dp[n-1][m-1];
+        return dp[m-1];
     }
 }
