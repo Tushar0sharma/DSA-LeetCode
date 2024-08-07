fc6c45996667702d2d4de6fa67a47c506ad35a0e
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-07 22:02:03 +0530

    Time: 51 ms (39.50%) | Memory: 51.2 MB (55.97%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -3,27 +3,21 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         int [][]dp=new int[n][m];
+        dp=grid;
+        int min=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                dp[i][j]=Integer.MAX_VALUE;
+                int mm=Integer.MAX_VALUE;
+                for(int k=0;i-1>=0 && k<m;k++){
+                    if(k!=j){
+                        mm=Math.min(mm,dp[i-1][k]);
+                    }
+                }
+                if(mm!=Integer.MAX_VALUE) dp[i][j]+=mm;
+                else dp[i][j]+=0;
+                if(i==n-1) min=Math.min(min,dp[n-1][j]);
             }
-        }
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<m;i++){
-            min=Math.min(min,call(0,i,dp,grid,n,m));
         }
         return min;
-    }
-    public int call(int i,int j,int [][]dp,int [][]ma,int n,int m){
-        if(i==n-1 && j>=0 && j<m) return dp[i][j]=ma[i][j];
-        if(j<0 || j>=m || i>=n) return Integer.MAX_VALUE;
-        if(dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
-        int min = Integer.MAX_VALUE;
-        for (int k = 0; k < n; k++) {
-            if (k != j) {
-                min = Math.min(min, call(i + 1, k, dp, ma, n,m));
-            }
-        }
-        return dp[i][j]=ma[i][j]+min;
     }
 }
