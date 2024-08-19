ca22c91bbba56822b9e6819b7a38671e32a25587
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-19 18:58:00 +0530

    Time: 43 ms (56.38%) | Memory: 46.6 MB (58.33%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -4,17 +4,20 @@ class Solution {
         for(int i:nums) sum+=i;
         int reqSum=sum/2;
         int n=nums.length;
+        // if(n==1) return false;
         if(sum%2!=0) return false;
-        Boolean [][]dp=new Boolean [n][reqSum+1];
-        return call(dp,nums,n-1,reqSum);
-    }
-    public boolean call(Boolean [][]dp,int []nums,int n,int sum){
-        if(sum==0) return true;
-        if(n==0) return nums[0]==sum;
-        if(dp[n][sum]!=null) return dp[n][sum];
-        boolean nottake=call(dp,nums,n-1,sum);
-        boolean take=false;
-        if(nums[n]<=sum) take=call(dp,nums,n-1,sum-nums[n]);
-        return dp[n][sum]=take||nottake;        
+        boolean [][]dp=new boolean [n][reqSum+1];
+        dp[0][0]=true;
+        if(nums[0]<=reqSum) dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            dp[i][0]=true;
+            for(int j=1;j<=reqSum;j++){
+                boolean nottake=dp[i-1][j];
+                boolean take=false;
+                if(nums[i]<=j) take=dp[i-1][j-nums[i]];
+                dp[i][j]=take||nottake;
+            }
+        }
+        return dp[n-1][reqSum];
     }
 }
