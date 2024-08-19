80d55832cebff97fdc71c73fefa3605ed8d66be9
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-19 10:59:46 +0530

    Time: 5 ms (64.07%) | Memory: 49.1 MB (18.88%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,11 +1,13 @@
 class Solution {
     public int minSteps(int n) {
         if(n==1) return 0;
-        return 1+call(1,n,1);
+        int [][]dp=new int[n+1][n/2+1];
+        return 1+call(1,n,1,dp);
     }
-    public int call(int i,int n,int j){
+    public int call(int i,int n,int j,int [][]dp){
         if(i==n) return 0;
         if(i>n) return 1000;
-        return Math.min(2+call(i*2,n,i),1+call(i+j,n,j));
+        if(dp[i][j]!=0) return dp[i][j];
+        return dp[i][j]=Math.min(2+call(i*2,n,i,dp),1+call(i+j,n,j,dp));
     }
 }
