58d074def096794add8a598075747b64012876e9
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-19 19:06:18 +0530

    Time: 39 ms (69.02%) | Memory: 44.6 MB (81.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -4,20 +4,21 @@ class Solution {
         for(int i:nums) sum+=i;
         int reqSum=sum/2;
         int n=nums.length;
-        // if(n==1) return false;
         if(sum%2!=0) return false;
-        boolean [][]dp=new boolean [n][reqSum+1];
-        dp[0][0]=true;
-        if(nums[0]<=reqSum) dp[0][nums[0]]=true;
+        boolean[]dp=new boolean [reqSum+1];
+        dp[0]=true;
+        if(nums[0]<=reqSum) dp[nums[0]]=true;
         for(int i=1;i<n;i++){
-            dp[i][0]=true;
+            boolean []dp1=new boolean [reqSum+1];
+            dp1[0]=true;
             for(int j=1;j<=reqSum;j++){
-                boolean nottake=dp[i-1][j];
+                boolean nottake=dp[j];
                 boolean take=false;
-                if(nums[i]<=j) take=dp[i-1][j-nums[i]];
-                dp[i][j]=take||nottake;
+                if(nums[i]<=j) take=dp[j-nums[i]];
+                dp1[j]=take||nottake;
             }
+            dp=dp1;
         }
-        return dp[n-1][reqSum];
+        return dp[reqSum];
     }
 }
