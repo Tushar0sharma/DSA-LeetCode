80dbd73cc317ce3f005371f3f18cd7a5aa307201
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-27 20:55:06 +0530

    Time: 14 ms (88.62%) | Memory: 45.3 MB (18.51%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,20 +1,22 @@
 class Solution {
     public int coinChange(int[] arr, int tar) {
         int n=arr.length;
-        int [][]dp=new int[n][tar+1];
-        dp[0][0]=0;
+        int []prev=new int[tar+1];
+        prev[0]=0;
         for(int i=1;i<=tar;i++){
-            if(i%arr[0]==0) dp[0][i]=i/arr[0];
-            else dp[0][i]=(int )1e9;
+            if(i%arr[0]==0) prev[i]=i/arr[0];
+            else prev[i]=(int )1e9;
         }
         for(int i=1;i<n;i++){
+            int []c=new int[tar+1];
             for(int j=0;j<=tar;j++){
-                int nottake=dp[i-1][j];
+                int nottake=prev[j];
                 int take=(int)1e9;
-                if(j>=arr[i]) take=1+dp[i][j-arr[i]];
-                dp[i][j]=Math.min(take,nottake);
+                if(j>=arr[i]) take=1+c[j-arr[i]];
+                c[j]=Math.min(take,nottake);
             }
+            prev=c;
         }
-        return dp[n-1][tar]==(int)1e9?-1:dp[n-1][tar];
+        return prev[tar]==(int)1e9?-1:prev[tar];
     }
 }
