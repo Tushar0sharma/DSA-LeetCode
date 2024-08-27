074034e43ce9c2d29465753a87cfb79563260cb2
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-27 09:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (29.31%) - LeetSync

--- 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.java
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.java
@@ -14,26 +14,16 @@
  * }
  */
 class Solution {
-    private int moves;
-
+    int cnt=0;
     public int distributeCoins(TreeNode root) {
-        moves = 0;
-        dfs(root);
-        return moves;
+        call(root);
+        return cnt;
     }
-
-    private int dfs(TreeNode current) {
-        if (current == null)
-            return 0;
-
-        // Calculate the coins each subtree has available to exchange
-        int leftCoins = dfs(current.left);
-        int rightCoins = dfs(current.right);
-
-        // Add the total number of exchanges to moves
-        moves += Math.abs(leftCoins) + Math.abs(rightCoins);
-
-        // The number of coins current has available to exchange
-        return (current.val - 1) + leftCoins + rightCoins;
+    public int call(TreeNode root){
+        if(root==null) return 0;
+        int a=call(root.left);
+        int b=call(root.right);
+        cnt+=Math.abs(a)+Math.abs(b);
+        return a+b+(root.val-1);
     }
 }
