0459ff9b6a6ee44948047f900858169319bac384
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-27 20:38:57 +0530

    Time: 14 ms (88.62%) | Memory: 44.4 MB (53.60%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,10 +1,24 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int [][]dp=new int[n][amount+1];
-        int a= call(n-1,coins,dp,amount);
-        if(a==(int)1e9) return -1;
-        return a;
+    public int coinChange(int[] arr, int tar) {
+        int n=arr.length;
+        int [][]dp=new int[n][tar+1];
+        // int a= call(n-1,coins,dp,amount);
+        // if(a==(int)1e9) return -1;
+        // return a;
+        dp[0][0]=0;
+        for(int i=1;i<=tar;i++){
+            if(i%arr[0]==0) dp[0][i]=i/arr[0];
+            else dp[0][i]=(int )1e9;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=tar;j++){
+                int nottake=dp[i-1][j];
+                int take=(int)1e9;
+                if(j>=arr[i]) take=1+dp[i][j-arr[i]];
+                dp[i][j]=Math.min(take,nottake);
+            }
+        }
+        return dp[n-1][tar]==(int)1e9?-1:dp[n-1][tar];
     }
     public int call(int i,int []arr,int [][]dp,int tar){
         if(i==0){
