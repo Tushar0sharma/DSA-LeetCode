ac7cbc8dcc2a2adb8bad5b4bb06ef3401802e83b
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-28 14:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (9.15%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/merge-two-binary-trees.java
@@ -18,16 +18,13 @@ class Solution {
         return call(root1,root2);        
     }
     public TreeNode call(TreeNode r1,TreeNode r2){
-        if(r1==null && r2==null) return null;
         if(r1==null) return r2;
-        if(r2==null) return r1;         
-        int a=0;
-        int b=0;
-        if(r1!=null) a=r1.val;
-        if(r2!=null) b=r2.val;
-        TreeNode root=new TreeNode(a+b);
-        root.left=call(r1.left,r2.left);
-        root.right=call(r1.right,r2.right);
-        return root;
+        if(r2==null) return r1;        
+        r1.val+=r2.val;
+        if(r2.left!=null)
+        r1.left=call(r1.left,r2.left);
+        if(r2.right!=null)
+        r1.right=call(r1.right,r2.right);
+        return r1;
     }
 }
