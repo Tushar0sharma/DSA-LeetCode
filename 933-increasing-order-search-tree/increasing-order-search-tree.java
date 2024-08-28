bbc4672b5d6dea52f8a4f7f019d151e6e14068fb
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-28 14:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (46.83%) - LeetSync

--- 933-increasing-order-search-tree/increasing-order-search-tree.java
+++ 933-increasing-order-search-tree/increasing-order-search-tree.java
@@ -14,21 +14,19 @@
  * }
  */
 class Solution {
-    public TreeNode increasingBST(TreeNode k) {
-        List<Integer>l=new ArrayList<>();
-        call(l,k);
-        TreeNode root=new TreeNode(l.get(0));
-        TreeNode ans=root;
-        for(int i=1;i<l.size();i++){
-            root.right=new TreeNode(l.get(i));
-            root=root.right;
-        }
-        return ans;
+    private TreeNode pre=null;
+    private TreeNode res=null;
+    public TreeNode increasingBST(TreeNode root) {
+        in(root);
+        return res;
     }
-    public void call(List<Integer>l,TreeNode r){
-        if(r==null) return ;
-        call(l,r.left);
-        l.add(r.val);
-        call(l,r.right);
+    public void in(TreeNode root){
+        if(root==null) return;
+        in(root.left);
+        if(res==null) res=root;
+        else pre.right=root;
+        pre=root;
+        root.left=null;
+        in(root.right);
     }
 }
