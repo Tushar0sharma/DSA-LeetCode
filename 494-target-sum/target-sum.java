d53f05a4cf23c631636e90b73edae5532e0aa49c
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-08-29 20:52:55 +0530

    Time: 5 ms (86.32%) | Memory: 43 MB (52.69%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,25 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return cal(nums,target,0,0);
+        int n=nums.length;
+        int sum=0;
+        for(int i:nums) sum+=i;
+        if((sum-target)%2!=0  || sum-target<0) return 0;
+        return cal(nums,(sum-target)/2,n);
     }
-    public int cal(int []nums,int t,int sum,int i){
-        if(i==nums.length){
-            if(sum==t){
-                return 1;
+    public int cal(int []nums,int t,int n){
+        int [][]dp=new int[n][t+1];
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        if(nums[0]==0) dp[0][0]=2;
+        else dp[0][0]=1;
+        if(nums[0]!=0 && nums[0]<=t) dp[0][nums[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=t;j++){
+                int nt=dp[i-1][j];
+                int tt=0;
+                if(j>=nums[i]) tt=dp[i-1][j-nums[i]];
+                dp[i][j]=tt+nt;
             }
-            return 0;
         }
-        int take1=cal(nums,t,sum+nums[i],i+1);
-        int take2=cal(nums,t,sum-nums[i],i+1);
-        return take1+take2;
+        return dp[n-1][t];
     }
 }
