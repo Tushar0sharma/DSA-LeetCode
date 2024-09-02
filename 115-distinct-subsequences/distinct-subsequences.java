1596b9e7f79de129924308d9b416cebadca0e758
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-02 20:45:18 +0530

    Time: 11 ms (90.39%) | Memory: 41.2 MB (98.60%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -3,19 +3,14 @@ class Solution {
         int n=s.length();
         int m=t.length();
         int []dp=new int[m+1];
-
         dp[0]=1;
-
         for(int i=1;i<=n;i++){
-            int []curr=new int[m+1];
-            curr[0]=1;
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    curr[j]=dp[j-1]+dp[j];
+                    dp[j]=dp[j-1]+dp[j];
                 }
-                else curr[j]=dp[j];
+                else dp[j]=dp[j];
             }
-            dp=curr;
         }
         return dp[m];
     }
