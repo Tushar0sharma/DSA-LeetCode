543f78dd3ba58ac4caa89baa413267ba50009842
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-02 20:01:25 +0530

    Time: 12 ms (85.60%) | Memory: 44.8 MB (89.71%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,25 +2,21 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
-        int [][]dp=new int[n+1][m+1];
+        int []dp=new int[m+1];
 
-        for(int i=0;i<=n;i++) dp[i][0]=1;
+        dp[0]=1;
 
         for(int i=1;i<=n;i++){
+            int []curr=new int[m+1];
+            curr[0]=1;
             for(int j=1;j<=m;j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    curr[j]=dp[j-1]+dp[j];
                 }
-                else dp[i][j]=dp[i-1][j];
+                else curr[j]=dp[j];
             }
+            dp=curr;
         }
-        return dp[n][m];
+        return dp[m];
     }
-    // public int call(int i,int j,int [][]dp,String s,String t){
-    //     if(j<0) return 1;
-    //     if(i<0) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     if(s.charAt(i)==t.charAt(j)) return dp[i][j]=call(i-1,j-1,dp,s,t)+call(i-1,j,dp,s,t);
-    //     else return dp[i][j]=call(i-1,j,dp,s,t);
-    // }
 }
