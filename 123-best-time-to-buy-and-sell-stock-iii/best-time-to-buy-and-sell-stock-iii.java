a92b33fd68a93593d8cc1452ee7bfde435785863
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-03 23:15:37 +0530

    Time: 166 ms (9.38%) | Memory: 81.5 MB (11.91%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] p) {
+        int n=p.length;
+        int [][][]dp=new int[n+1][2][3];
+        for(int [][]i:dp) for(int []j:i) Arrays.fill(j,-1);
+        return call(dp,0,1,2,p);
+    }
+    public int call(int [][][]dp,int i,int j,int c,int []p){
+        if(c==0) return 0;
+        if(i==p.length) return 0;
+        if(dp[i][j][c]!=-1) return dp[i][j][c];
+        if(j==1){
+            return dp[i][j][c]=Math.max(-p[i]+call(dp,i+1,0,c,p),call(dp,i+1,1,c,p));
+        }
+        else{
+            return dp[i][j][c]=Math.max(p[i]+call(dp,i+1,1,c-1,p),call(dp,i+1,0,c,p));
+        }
+    }
+}
