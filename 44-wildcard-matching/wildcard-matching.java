ebcd2de76c8b9491ea268817a3e4184e027102c1
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-03 12:21:21 +0530

    Time: 28 ms (12.23%) | Memory: 44.3 MB (91.56%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,27 +1,52 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
+    public boolean isMatch(String s, String t) {
         int n=s.length();
-        int m=p.length();
-        Boolean [][]dp=new Boolean[n+1][m+1];
-        return call(n,m,s,p,dp);
-    }
-    public boolean  call(int i,int j,String s,String t,Boolean [][]dp){
-        if(i==0 && j==0) return true;
-        if(i==0 && j>0) {
-            for(int k=1;k<=j;k++){
-                if(t.charAt(k-1)!='*') return false;
+        int m=t.length();
+        boolean [][]dp=new boolean[n+1][m+1];
+        for(int i=1;i<=m;i++){
+            boolean f=false;
+            for(int k=1;k<=i;k++){
+                 if(t.charAt(k-1)!='*'){
+                    f=true;
+                    dp[0][i]=false;
+                 }
+                 if(!f) dp[0][i]=true;
             }
-            return true;
         }
-        if(j==0 && i>0) return false;
-        if (dp[i][j] != null) return dp[i][j];
+        dp[0][0]=true;
 
-        if(s.charAt(i-1)==t.charAt(j-1) || t.charAt(j-1)=='?'){
-            return dp[i][j]= call(i-1,j-1,s,t,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1) || t.charAt(j-1)=='?'){
+                     dp[i][j]=dp[i-1][j-1];
+                }
+                else if(t.charAt(j-1)=='*'){
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
         }
-        else if(t.charAt(j-1)=='*'){
-            return dp[i][j] =call(i-1,j,s,t,dp)||call(i,j-1,s,t,dp);
-        }
-        return false;
+        return dp[n][m];
     }
+    // public boolean  call(int i,int j,String s,String t,Boolean [][]dp){
+    //     if(i==0 && j==0) return true;
+    //     if(i==0 && j>0) {
+    //         for(int k=1;k<=j;k++){
+    //             if(t.charAt(k-1)!='*') return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(j==0 && i>0) return false;
+    //     if (dp[i][j] != null) return dp[i][j];
+
+    //     if(s.charAt(i-1)==t.charAt(j-1) || t.charAt(j-1)=='?'){
+    //         return dp[i][j]= call(i-1,j-1,s,t,dp);
+    //     }
+    //     else if(t.charAt(j-1)=='*'){
+    //         return dp[i][j] =call(i-1,j,s,t,dp)||call(i,j-1,s,t,dp);
+    //     }
+    //     return false;
+    // }
 }
