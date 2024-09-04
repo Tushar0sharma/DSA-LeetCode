ea83fdd8ae8e5990ea2b59c5a45e154e15ab1cec
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-04 21:49:21 +0530

    Time: 1 ms (84.58%) | Memory: 41.5 MB (71.97%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,18 +1,23 @@
 class Solution {
     public int maxProfit(int[] p) {
         int n=p.length;
-        int [][]dp=new int[n][2];
-        for(int []i:dp) Arrays.fill(i,-1);
-        return call(0,1,p,dp);
-    }
-    public int call(int i,int j,int[]p,int [][]dp){
-        if(i>=p.length) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(j==1){
-            return dp[i][j]=Math.max(-p[i]+call(i+1,0,p,dp),call(i+1,1,p,dp));
+        int [][]dp=new int[n+2][2];
+        // for(int []i:dp) Arrays.fill(i,-1);
+        // return call(0,1,p,dp);
+        for(int i=n-1;i>=0;i--){
+            dp[i][1]=Math.max(-p[i]+dp[i+1][0],dp[i+1][1]);
+            dp[i][0]=Math.max(p[i]+dp[i+2][1],dp[i+1][0]);            
         }
-        else{
-            return dp[i][j]=Math.max(p[i]+call(i+2,1,p,dp),call(i+1,0,p,dp));
-        }
+        return dp[0][1];
     }
+    // public int call(int i,int j,int[]p,int [][]dp){
+    //     if(i>=p.length) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(j==1){
+    //         return dp[i][j]=Math.max(-p[i]+call(i+1,0,p,dp),call(i+1,1,p,dp));
+    //     }
+    //     else{
+    //         return dp[i][j]=Math.max(p[i]+call(i+2,1,p,dp),call(i+1,0,p,dp));
+    //     }
+    // }
 }
