b4e7eb712639fe7b6cf9da9d122b225b38d79b71
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-07 16:03:06 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (81.68%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,16 +1,23 @@
 class Solution {
     public int numTrees(int n) {
         int []dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return call(n,dp);
-    }
-    public int call(int i,int []dp){
-        if(i<=1) return 1;
-        if(dp[i]!=-1) return dp[i];
-        int ans=0;
-        for(int j=1;j<=i;j++){
-            ans+=call(j-1,dp)*call(i-j,dp);
+        // Arrays.fill(dp,-1);
+        // return call(n,dp);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
         }
-        return dp[i]=ans;
+        return dp[n];
     }
+    // public int call(int i,int []dp){
+    //     if(i<=1) return 1;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int ans=0;
+    //     for(int j=1;j<=i;j++){
+    //         ans+=call(j-1,dp)*call(i-j,dp);
+    //     }
+    //     return dp[i]=ans;
+    // }
 }
