80145d77ef0d64eb6903dea5912ee304ef0e8a59
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-19 18:23:31 +0530

    Time: 1 ms (99.68%) | Memory: 41.4 MB (98.16%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,10 +1,15 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer>[][]dp=new ArrayList[expression.length()][expression.length()];
+        return call(expression,0,expression.length()-1,dp);
+    }
+    public List<Integer> call(String expression,int l1,int l2,List<Integer>[][]dp){        
         List<Integer>l=new ArrayList<>();
-        for(int i=0;i<expression.length();i++){
+        if(dp[l1][l2]!=null) return dp[l1][l2];
+        for(int i=l1;i<=l2;i++){
             if(expression.charAt(i)=='+'||expression.charAt(i)=='-'||expression.charAt(i)=='*'){
-                List<Integer>left=diffWaysToCompute(expression.substring(0,i));
-                List<Integer>right=diffWaysToCompute(expression.substring(i+1));
+                List<Integer>left=call(expression,l1,i-1,dp);
+                List<Integer>right=call(expression,i+1,l2,dp);
                 
                 for(Integer s:left){
                     for(Integer t:right){
@@ -21,7 +26,8 @@ class Solution {
                 }
             }
         }
-        if(l.size()==0) l.add(Integer.parseInt(expression));
-        return l;
+        if(l.size()==0) l.add(Integer.parseInt(expression.substring(l1,l2+1)));
+        dp[l1][l2]=l;
+        return dp[l1][l2];
     }
 }
