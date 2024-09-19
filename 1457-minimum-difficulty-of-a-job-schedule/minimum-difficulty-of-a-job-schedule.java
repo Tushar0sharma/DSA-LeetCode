1208b5fa5a4e5104d4765b9d883d17b07e125c80
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-19 20:13:59 +0530

    Time: 11 ms (58.86%) | Memory: 41.7 MB (43.12%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -5,20 +5,20 @@ class Solution {
         int [][]dp=new int[n][d+1];
         for(int []i:dp) Arrays.fill(i,-1);
         return call(0,d,jobDifficulty,dp);
+
     }
     public int call(int idx,int d,int[]nums,int [][]dp){
-        if(d==1){
-            int max=nums[idx];
-            for(int i=idx;i<nums.length;i++) max=Math.max(max,nums[i]);
-            return max;
-        }
+        if(d==0 && idx==nums.length) return 0;
+        if(d==0 || idx==nums.length) return Integer.MAX_VALUE;
+        
         if(dp[idx][d]!=-1) return dp[idx][d];
         int f=Integer.MAX_VALUE;
         int max=0;
-        for(int i=idx;i<=nums.length-d;i++){
+        for(int i=idx;i<nums.length;i++){
              max=Math.max(max,nums[i]);
-            int res=max+call(i+1,d-1,nums,dp);
-            f=Math.min(f,res);
+            int res=call(i+1,d-1,nums,dp);
+            if(res!=Integer.MAX_VALUE)
+                f=Math.min(f,res+max);
         }
         return dp[idx][d]=f;
     }
