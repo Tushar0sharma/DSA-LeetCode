c61c067544402d82c3e10c3c83e3dcd809074e0f
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-09-25 13:58:17 +0530

    Time: 352 ms (7.73%) | Memory: 46.1 MB (5.89%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,31 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        // int []dp=new int[n];
-        // dp[n-1]=true;
-        int goal=n-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]+i>=goal)   goal=i;
-        }
-        return goal==0;
+        int []dp=new int[n];
+        Arrays.fill(dp,-1);
+        return call(dp,nums,0);
     }
+    public boolean call(int []dp,int []nums,int i){
+        if(i>=nums.length-1){
+            return true;
+        }
+        if(nums[i]==0){
+            dp[i]=0;
+            return false;
+        }
+        if(dp[i]!=-1){
+            return dp[i]==1;
+        }
+        int jump=nums[i];
+        for(int k=1;k<=jump;k++){
+            if(call(dp,nums,i+k)){
+                dp[i]=1;
+                return true;
+            }
+        }
+        dp[i]=0;
+        return false;
+    }
+
+
 }
