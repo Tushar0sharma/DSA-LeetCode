250e923b32129ba6776aec387aa74015259e1a3f
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-05 09:39:45 +0530

    Time: 6 ms (86.36%) | Memory: 42.3 MB (94.64%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,26 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int []a=new int[26];
+        int []b=new int[26];
         int n=s1.length();
         int m=s2.length();
         if(n>m) return false;
-        int cnt[]=new int[26];
         for(int i=0;i<n;i++){
-            cnt[s1.charAt(i)-'a']++;
-            cnt[s2.charAt(i)-'a']--;
+            a[s1.charAt(i)-'a']++;
+            b[s2.charAt(i)-'a']++;
         }
-        if(check(cnt)) return true;
-        int j=0;
+        if(c(a,b)) return true;
         for(int i=n;i<m;i++){
-            cnt[s2.charAt(i)-'a']--;
-            cnt[s2.charAt(j++)-'a']++;
-            if(check(cnt)) return true;
+            b[s2.charAt(i-n)-'a']--;
+            b[s2.charAt(i)-'a']++;
+            if(c(a,b)) return true;
         }
-        return false;
+        return false;        
     }
-    public boolean check(int []cnt){
-        for(int i=0;i<26;i++){
-            if(cnt[i]!=0) return false;
-        }
+    public boolean c(int []a,int []b){
+        for(int i=0;i<26;i++) if(a[i]!=b[i]) return false;
         return true;
     }
 }
