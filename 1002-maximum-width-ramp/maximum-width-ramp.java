59da3e1b41ec667d332d2bb57c34c2103e20edb9
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-10 16:09:22 +0530

    Time: 4 ms (84.83%) | Memory: 55 MB (29.60%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,17 +1,20 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
         int n=nums.length;
-        Stack<Integer>s=new Stack<>();
-        for(int i=0;i<n;i++){
-            if(s.isEmpty()|| nums[s.peek()]>nums[i]){
-                s.push(i);
-            }
-        }
+        int []min=new int[n],max=new int[n];
+        min[0]=nums[0];
+        max[n-1]=nums[n-1];
+        for(int i=1;i<n;i++) min[i]=Math.min(min[i-1],nums[i]);
+        for(int i=n-2;i>=0;i--) max[i]=Math.max(max[i+1],nums[i]);
+        int i=0;
+        int j=0;
         int ans=0;
-        for(int i=n-1;i>=0;i--){
-            while(!s.isEmpty() && nums[s.peek()]<=nums[i]){
-                ans=Math.max(ans,i-s.pop());
+        while(j<n){
+            if(max[j]>=min[i]){
+                ans=Math.max(ans,j-i);
+                j++;
             }
+            else i++;
         }
         return ans;
     }
