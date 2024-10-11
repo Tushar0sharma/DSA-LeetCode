555316ad41609e6bff74ea8a3cd35684265f0fa1
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-11 22:00:56 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (6.82%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,29 +1,46 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode d = new ListNode();
-        ListNode ans=d;
-        
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->a.val-b.val);
-        for (ListNode l : lists) {
-            if (l != null)
-                pq.add(l);
+        return call(lists,0,lists.length-1);
+    }
+    public ListNode call(ListNode []list,int i,int j){
+        if(i==j){
+            return list[i];
         }
-        while(!pq.isEmpty()){
-            ListNode p=pq.poll();
-            d.next=p;
+        else if(i<j){
+            int mid=(i+j)/2;
+            ListNode l=call(list,i,mid);
+            ListNode r=call(list,mid+1,j);
+            return merge(l,r);
+        }
+        else{
+            return null;
+        }
+    }
+    public ListNode merge(ListNode l,ListNode r){
+        ListNode ans=new ListNode();
+        ListNode d=ans;
+        while(l!=null && r!=null){
+            if(l.val<r.val){
+                d.next=new ListNode(l.val);
+                l=l.next;
+            }
+            else{
+                d.next=new ListNode(r.val);
+                r=r.next;
+            }
             d=d.next;
-            if(p.next!=null) pq.add(p.next);
         }
-        return ans.next;        
+        d.next=(l!=null)?l:r;
+        return ans.next;
     }
 }
