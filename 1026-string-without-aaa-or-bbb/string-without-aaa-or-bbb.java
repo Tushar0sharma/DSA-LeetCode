309ba6c314a2f05cbe0972f0cb1579c073d8063b
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-17 21:33:54 +0530

    Time: 4 ms (6.60%) | Memory: 41 MB (42.71%) - LeetSync

--- 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.java
+++ 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.java
@@ -12,21 +12,21 @@ class Solution {
         if(a>0) pq.add(new pair(a,'a'));
         if(b>0) pq.add(new pair(b,'b'));
 
-        String ans="";
+        StringBuilder ans=new StringBuilder();
         while(!pq.isEmpty()){
             pair p=pq.poll();
             if(ans.length()>1 && ans.charAt(ans.length()-1)==p.ch && ans.charAt(ans.length()-2)==p.ch){
-                if(pq.isEmpty()) return ans;
+                if(pq.isEmpty()) return ans.toString();
                 pair q=pq.poll();
-                ans+=q.ch;
+                ans.append(q.ch);
                 if(q.val-1>0) pq.add(new pair(q.val-1,q.ch));
                 pq.add(p);
             }
             else{
-                ans+=p.ch;
+                ans.append(p.ch);
                 if(p.val-1>0) pq.add(new pair(p.val-1,p.ch));
             }
         }        
-        return ans;
+        return ans.toString();
     }
 }
