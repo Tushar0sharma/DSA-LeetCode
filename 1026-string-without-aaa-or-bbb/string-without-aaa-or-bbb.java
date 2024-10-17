cc321df3fdb50da2c8105d6dc07ca23583ccafc8
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-17 21:30:12 +0530

    Time: 11 ms (6.60%) | Memory: 41.9 MB (7.64%) - LeetSync

---
+++ 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.java
@@ -0,0 +1,32 @@
+class pair{
+    int val;
+    char ch;
+    pair(int val,char ch){
+        this.val=val;
+        this.ch=ch;
+    }
+}
+class Solution {
+    public String strWithout3a3b(int a, int b) {
+        PriorityQueue<pair>pq=new PriorityQueue<>((x,y)->y.val-x.val);
+        if(a>0) pq.add(new pair(a,'a'));
+        if(b>0) pq.add(new pair(b,'b'));
+
+        String ans="";
+        while(!pq.isEmpty()){
+            pair p=pq.poll();
+            if(ans.length()>1 && ans.charAt(ans.length()-1)==p.ch && ans.charAt(ans.length()-2)==p.ch){
+                if(pq.isEmpty()) return ans;
+                pair q=pq.poll();
+                ans+=q.ch;
+                if(q.val-1>0) pq.add(new pair(q.val-1,q.ch));
+                pq.add(p);
+            }
+            else{
+                ans+=p.ch;
+                if(p.val-1>0) pq.add(new pair(p.val-1,p.ch));
+            }
+        }        
+        return ans;
+    }
+}
