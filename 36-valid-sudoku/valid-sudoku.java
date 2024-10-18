0227d02a85d486e970f4249a52108bd9aa62b279
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-18 21:16:25 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (27.24%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,35 +1,34 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return solve(board); 
+        return call(board);
     }
-    public boolean solve(char[][]board){
+    public boolean call(char [][]board){
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char num=board[i][j];
                 if(board[i][j]!='.'){
                     board[i][j]='.';
-                    if(!safe(i,j,board,num)){
-                        return false;
-                    }
+                    if(!check(board,num,i,j)) return false;
                     board[i][j]=num;
                 }
             }
         }
         return true;
     }
-    public boolean safe(int row,int col,char[][]board,char num){
+    public boolean check(char [][]b,char k,int r,int c){
         for(int i=0;i<9;i++){
-            if(board[i][col]==num) return false;
-            if(board[row][i]==num) return false;
+            if(b[i][c]==k) return false;
+            if(b[r][i]==k) return false;
         }
-        int start=row-row%3;
-        int end=col-col%3;
+        int row=r-r%3;
+        int col=c-c%3;
 
-        for(int i=start;i<start+3;i++){
-            for(int j=end;j<end+3;j++){
-                if(board[i][j]==num) return false;
-            }
+        for(int i=row;i<row+3;i++){
+            for(int j=col;j<col+3;j++){
+                if(b[i][j]==k) return false;
+            }   
         }
         return true;
+        
     }
 }
