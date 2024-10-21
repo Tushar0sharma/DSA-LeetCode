5574d4ca6b7b28ed3c031493d4ef29ce72c5c844
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-21 08:40:09 +0530

    Time: 544 ms (5.23%) | Memory: 44.8 MB (84.31%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
@@ -2,22 +2,22 @@ class Solution {
     int max=0;
     public int maxUniqueSplit(String s) {
         Set<String>st=new HashSet<>();
-        call(st,s);
+        call(s,st);
         return max;
     }
-    public void call(Set<String>st,String s){
+    public void call(String s,Set<String>st){
         if(s.length()==0){
+            System.out.println(st);
             max=Math.max(max,st.size());
             return;
         }
         for(int i=1;i<=s.length();i++){
-            String k=s.substring(0,i);
-            if(!st.contains(k)){
-                st.add(k);
-                call(st,s.substring(i));
-                st.remove(k);
+            String a=s.substring(0,i);
+            if(!st.contains(a)){
+                st.add(a);
+                call(s.substring(i),st);
+                st.remove(a);
             }
         }
-        return;
     }
 }
