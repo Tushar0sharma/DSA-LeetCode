6100f88cf743d7e8a2b01ca4d5861f6a9f0b124a
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-29 09:29:06 +0530

    Time: 5 ms (100.00%) | Memory: 57.4 MB (11.59%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
@@ -2,21 +2,18 @@ class Solution {
     public int maxMoves(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int [][]dp=new int[n+1][m+1];
+        int [][]dp=new int[n][m];
+        for(int []i:dp){
+            Arrays.fill(i,-1);
+        }
         int max=0;
         for(int i=0;i<n;i++){
             max=Math.max(max,call(grid,dp,i,0,n,m));
         }
-        for(int i[]:dp){
-            for(int j:i){
-                System.out.print(j+" ");
-            }
-            System.out.println();
-        }
         return max;
     }
     public int call(int [][]grid,int [][]dp,int i,int j,int n,int m){
-        if(dp[i][j]!=0) return dp[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
         
         int c=0;
         for(int k=-1;k<=1;k++){
