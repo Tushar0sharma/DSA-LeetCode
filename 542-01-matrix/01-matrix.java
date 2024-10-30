afd43ac2e17d0206fbbbd228de83ae5f347d6c71
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-10-30 16:10:26 +0530

    Time: 13 ms (87.59%) | Memory: 46.6 MB (93.07%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,51 +1,31 @@
-class pair{
-    int row;
-    int col;
-    int steps;
-    pair(int row,int col,int steps){
-        this.row=row;
-        this.col=col;
-        this.steps=steps;
-    }
-}
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
-        int [][]vis=new int[n][m];
-        int [][]dist=new int[n][m];
-
-        Queue<pair>q=new LinkedList<>();
+        boolean [][]vis=new boolean[n][m];
+        Queue<int[]>q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    vis[i][j]=1;
-                    q.add(new pair(i,j,0));
+                    vis[i][j]=true;
+                    q.add(new int[]{i,j,0});
                 }
-                else vis[i][j]=0;
             }
         }
-
         int []r={0,0,-1,1};
         int []c={1,-1,0,0};
-
         while(!q.isEmpty()){
-            int row=q.peek().row;
-            int col=q.peek().col;
-            int steps=q.peek().steps;
-            q.poll();
-            dist[row][col]=steps;
-
+            int []p=q.poll();
+            mat[p[0]][p[1]]=p[2];
             for(int i=0;i<4;i++){
-                int nr=r[i]+row;
-                int nc=c[i]+col;
-
-                if(nr>=0 && nc>=0 && nr<n && nc<m && vis[nr][nc]==0){
-                    vis[nr][nc]=1;
-                    q.add(new pair(nr,nc,steps+1));
+                int nr=p[0]+r[i];
+                int nc=p[1]+c[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc]){
+                    vis[nr][nc]=true;
+                    q.add(new int[]{nr,nc,p[2]+1});
                 }
             }
         }
-        return dist;
+        return mat;
     }
 }
