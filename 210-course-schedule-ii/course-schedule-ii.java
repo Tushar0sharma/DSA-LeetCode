cb37dd83fa1677c83dc5d85f5d76c1be24ffc5fc
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-06 21:25:05 +0530

    Time: 5 ms (78.23%) | Memory: 46.1 MB (28.01%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,38 +1,28 @@
 class Solution {
-    public int[] findOrder(int n, int[][] p) {
+    public int[] findOrder(int n, int[][] prerequisites) {
         List<List<Integer>>ll=new ArrayList<>();
+        int []indeg=new int[n];
         for(int i=0;i<n;i++) ll.add(new ArrayList<>());
-        for(int[] i:p) {
+        for(int []i:prerequisites){
             ll.get(i[1]).add(i[0]);
+            indeg[i[0]]++;
         }
-        int[]vis=new int[n];
-        boolean[]cycle={false};
-        List<Integer>order=new ArrayList<>();
+        Queue<Integer>q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(vis,order,ll,cycle,i); 
-                if(cycle[0]) return new int[0];
-            }
+            if(indeg[i]==0) q.add(i);
         }
         int []ans=new int[n];
-        // int k=0;
-        for(int i:order){
-            ans[--n]=i;
+        int k=0;
+        while(!q.isEmpty()){
+            int p=q.poll();
+            ans[k++]=p;
+            for(int j:ll.get(p)){
+                if(--indeg[j]==0){
+                    q.add(j);
+                }
+            }
         }
+        if(k!=n) return new int[]{};
         return ans;
-    }
-    public void dfs(int []vis,List<Integer>order,List<List<Integer>>ll,boolean[]cycle,int i){
-        vis[i]=1;
-        for(int k:ll.get(i)){
-            if(vis[k]==0){
-                dfs(vis,order,ll,cycle,k);
-            }
-            else if(vis[k]==1){
-                cycle[0]=true;
-                return;
-            }
-        }
-        vis[i]=2;
-        order.add(i);        
     }
 }
