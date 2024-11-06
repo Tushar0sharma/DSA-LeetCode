b78beb3c223163bd2d42cb5287ce837ae1201273
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-06 19:26:12 +0530

    Time: 6 ms (74.60%) | Memory: 45.4 MB (48.97%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,22 +1,24 @@
 class Solution {
-    public boolean canFinish(int n, int[][] p) {
-        List<List<Integer>>ll=new ArrayList<>();
-        int []deg=new int[n];
-        for(int i=0;i<n;i++) ll.add(new ArrayList<>());
-        for(int []i:p){
-            ll.get(i[1]).add(i[0]);
-            deg[i[0]]++;
-        }
-        List<Integer>order=new ArrayList<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int ans=0;
         Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<n;i++) if(deg[i]==0) q.add(i);
+        int []indeg=new int[numCourses];
+        List<List<Integer>>ll=new ArrayList<>();
+        for(int i=0;i<numCourses;i++) ll.add(new ArrayList<>());
+        for(int[]i:prerequisites){
+            ll.get(i[1]).add(i[0]);
+            indeg[i[0]]++;
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.add(i);
+        }
         while(!q.isEmpty()){
-            int w=q.poll();
-            order.add(w);
-            for(int s:ll.get(w)){
-                if(--deg[s]==0) q.add(s);
+            int u=q.poll();
+            ans++;
+            for(int i:ll.get(u)){
+                if(--indeg[i]==0) q.add(i);
             }
         }
-        return order.size()==n;
+        return ans==numCourses;
     }
 }
