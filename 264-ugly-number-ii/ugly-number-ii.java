e646aada37814552d9b81bf5762c3afadbfee0f6
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-10 20:42:27 +0530

    Time: 50 ms (25.99%) | Memory: 44.8 MB (9.54%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,19 +1,20 @@
 class Solution {
     public int nthUglyNumber(int n) {
         PriorityQueue<Long>pq=new PriorityQueue<>();
-        pq.add(1L);
         Set<Long>s=new HashSet<>();
-        long ans=0;
         int []prime={2,3,5};
+        pq.add(1L);
+        long ans=1;
         for(int i=0;i<n;i++){
-            long val=pq.poll();
-            ans=val;
-            for(int k:prime){
-                long a=val*k;
-                if(s.add(a))
-                pq.add(a);
+            long v=pq.poll();
+            ans=v;
+            for(int j:prime){
+                long k=j*v;
+                if(s.add(k)){
+                    pq.add(k);
+                }
             }
         }
-        return (int)ans;
+        return (int)ans;    
     }
 }
