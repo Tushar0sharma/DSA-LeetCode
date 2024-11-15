a20e5e3b4af901d3e724d4d97ea6927fe80d07d1
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-15 13:53:05 +0530

    Time: 6 ms (86.38%) | Memory: 44.2 MB (74.59%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,41 +1,32 @@
-class pair{
-    int st;
-    int stop;
-    int dist;
-    pair(int st,int dist,int stop){
-        this.st=st;
-        this.dist=dist;
-        this.stop=stop;
-    }
-}
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        PriorityQueue<pair>pq=new PriorityQueue<>((a,b)->a.dist-b.dist);
+        int m=flights.length;
         List<List<int[]>>ll=new ArrayList<>();
-        for(int i=0;i<n;i++) ll.add(new ArrayList<>());
-        for(int []i:flights){
+        Queue<int[]>q=new LinkedList<>();
+        q.add(new int[]{src,0,0});
+        int []dist=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
+        for(int i=0;i<n;i++){
+            ll.add(new ArrayList<>());
+        }
+        for(int[] i:flights){
             ll.get(i[0]).add(new int[]{i[1],i[2]});
         }
-        int [][]mindist=new int[n][k+2];
-        for(int i=0;i<n;i++) Arrays.fill(mindist[i],Integer.MAX_VALUE);
-        pq.add(new pair(src,0,0));
-        while(!pq.isEmpty()){
-            pair p=pq.poll();
-            int node=p.st;
-            int d=p.dist;
-            int st=p.stop;
-            if(node==dst ) return d;
-            if(st>k) continue; 
-
-            for(int[] i:ll.get(node)){
-                int dest=i[0];
-                int dist=i[1];
-                if(d+dist<mindist[dest][st+1]){
-                    mindist[dest][st+1]=d+dist;
-                    pq.add(new pair(dest,d+dist,st+1));
+        while(!q.isEmpty()){
+            int []p=q.poll();
+            int node=p[0];
+            int cost=p[1];
+            int stop=p[2];
+            if(stop>k) continue;
+            for(int i[]:ll.get(node)){
+                int nt=i[1]+cost;
+                if(dist[i[0]]>nt){
+                    dist[i[0]]=nt;
+                    q.add(new int[]{i[0],nt,stop+1});
                 }
             }
         }
-        return -1;
+        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
     }
 }
