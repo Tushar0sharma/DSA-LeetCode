1e3d92944474a34d719088c66bf062629ec002e9
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-16 21:59:31 +0530

    Time: 11 ms (57.84%) | Memory: 44.5 MB (36.90%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,40 +1,30 @@
-class pair{
-    int row;
-    int col;
-    int k;
-    pair(int row,int col,int k){
-        this.row=row;
-        this.col=col;
-        this.k=k;
-    }
-}
 class Solution {
     public int swimInWater(int[][] grid) {
-        PriorityQueue<pair>q=new PriorityQueue<>((a,b)->a.k-b.k);
-        q.add(new pair(0,0,grid[0][0]));
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[2]-b[2]);
+        pq.add(new int[]{0,0,grid[0][0]});
         int n=grid.length;
-        int m=grid[0].length;
-        boolean [][]vis=new boolean[n][m];
-        vis[0][0]=true;
-        int min=0;
-        while(!q.isEmpty()){
-            pair p=q.poll();
-            int r=p.row;
-            int c=p.col;
-            int k=p.k;
-            if(r==n-1 && c==m-1){
-                min=k;
-                break;
-            }
-            int [][]dir={{0,1},{1,0},{-1,0},{0,-1}};
+        int [][]dist=new int[n][n];
+        for(int []i:dist) Arrays.fill(i,Integer.MAX_VALUE);
+        dist[0][0]=0;
+        int [][]dir={{0,1},{1,0},{-1,0},{0,-1}};
+        while(!pq.isEmpty()){
+            int []p=pq.poll();
+            int r=p[0];
+            int c=p[1];
+            int cost=p[2];
+            if(r==n-1 && c==n-1) return cost;
             for(int []i:dir){
-                int nr=i[0]+r;
-                int nc=i[1]+c;
-                if(nr<0 ||nr==n || nc<0 || nc==m || vis[nr][nc]) continue;
-                vis[nr][nc]=true;
-                q.add(new pair(nr,nc,Math.max(k,grid[nr][nc])));
-            }
+                int nr=r+i[0];
+                int nc=c+i[1];
+                if(nr>=0  && nc>=0 && nc<n && nr<n){
+                    int nt=Math.max(cost,grid[nr][nc]);
+                    if(dist[nr][nc]>nt){
+                        dist[nr][nc]=nt;
+                        pq.add(new int[]{nr,nc,nt});
+                    }     
+                }
+            }            
         }
-        return min;
+        return -1;
     }
 }
