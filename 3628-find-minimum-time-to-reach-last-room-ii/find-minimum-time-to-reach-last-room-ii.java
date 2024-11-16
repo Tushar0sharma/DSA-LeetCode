cdbd9ebe4f8e8575fedccd6ab0094e01eb73d474
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-16 22:06:54 +0530

    Time: 176 ms (93.51%) | Memory: 111.9 MB (39.44%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -3,9 +3,9 @@ class Solution {
         PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
         int n=moveTime.length;
         int m=moveTime[0].length;
-        int [][]dist=new int[n][m];
-        for(int []i:dist) Arrays.fill(i,Integer.MAX_VALUE);
-        dist[0][0]=0;
+        boolean [][]vis=new boolean[n][m];
+        // for(int []i:vis) Arrays.fill(i,Integer.MAX_VALUE);
+        vis[0][0]=true;
         pq.add(new int[]{0,0,0,1});
         int [][]dir={{0,1},{1,0},{-1,0},{0,-1}};
         while(!pq.isEmpty()){
@@ -18,13 +18,14 @@ class Solution {
             for(int []d:dir){
                 int nr=r+d[0];
                 int nc=c+d[1];
-                if(nr>=0 && nc>=0 && nr<n && nc<m){
+                if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc]){
                     int nt=Math.max(moveTime[nr][nc],t)+k;
                     int f=k==1?2:1;
-                    if(dist[nr][nc]>nt){
-                        dist[nr][nc]=nt;
+                    vis[nr][nc]=true;
+                    // if(vis[nr][nc]>nt){
+                        // vis[nr][nc]=nt;
                         pq.add(new int[]{nt,nr,nc,f});
-                    }
+                    // }
                 }
             }
         }
