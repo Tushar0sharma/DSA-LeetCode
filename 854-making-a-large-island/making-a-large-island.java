77f367655eadd136edf8b8c83484e49c7b2de0ee
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-16 19:56:26 +0530

    Time: 134 ms (39.09%) | Memory: 82.3 MB (25.60%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -1,70 +1,72 @@
 class Solution {
     public int largestIsland(int[][] grid) {
         int n=grid.length;
-        int []par=new int[n*n];
-        int []rank=new int[n*n];
-        for(int i=0;i<n*n;i++){
+        int m=grid[0].length;
+        int []par=new int[n*m+1];
+        int []size=new int[n*m+1];
+        for(int i=0;i<n*m+1;i++){
             par[i]=i;
-            rank[i]=1;
-        }  
-        int []r={0,0,1,-1};
-        int []c={1,-1,0,0};      
+            size[i]=1;
+        }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    for(int k=0;k<4;k++){
-                        int ni=i+r[k];
-                        int nj=j+c[k];
-                        if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj]==1){
-                            int node=i*n+j;
-                            int adj=ni*n+nj;
-                            union(node,adj,par,rank);
+                    int [][]dir={{0,1},{1,0},{0,-1},{-1,0}};
+                    for(int []k:dir){
+                        int nr=i+k[0];
+                        int nc=k[1]+j;
+                        int rr=i*n+j;
+                        int cc=nr*n+nc;
+                        if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+                            union(rr,cc,par,size);
                         }
                     }
                 }
             }
         }
-        int max=0;
-
+        int nas=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==0){
                     Set<Integer>s=new HashSet<>();
-                    for(int k=0;k<4;k++){
-                        int ni=i+r[k];
-                        int nj=j+c[k];
-                        if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj]==1){
-                            // int node=i*n+j;
-                            int adj=ni*n+nj;
-                            s.add(find(adj,par));
+                    int [][]dir={{0,1},{1,0},{0,-1},{-1,0}};
+                    for(int []k:dir){
+                        int nr=i+k[0];
+                        int nc=k[1]+j;
+                        int rr=i*n+j;
+                        int cc=nr*n+nc;
+                        if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+                            s.add(find(cc,par));
                         }
                     }
-                    int cnt=1;
-                    for(int g:s){
-                        cnt+=rank[g];
+                    int ans=0;
+                    for(int jj:s){
+                        ans+=size[jj];
                     }
-                    max=Math.max(max,cnt);
+                    nas=Math.max(nas,ans+1);
                 }
             }
         }
-        return max==0?n*n:max;
+        for(int i=0;i<n*n;i++){
+            nas=Math.max(nas,size[i]);
+        }
+        return nas;
     }
-    public int find(int u,int []par){
-        if(u==par[u]) return u;
-        return par[u]=find(par[u],par);
+    public int find(int p,int []par){
+        if(p==par[p]) return p;
+        return par[p]=find(par[p],par);
     }
-    public void union(int u,int v,int []par,int []rank){
+    public void union(int u,int v,int []par,int []size){
         int pu=find(u,par);
         int pv=find(v,par);
-
-        if(pu==pv) return ;
-        if(rank[pu]>rank[pv]){
-            rank[pu]+=rank[pv];
+        if(pu==pv) return;
+        if(size[pu]>size[pv]){
+            size[pu]+=size[pv];
             par[pv]=pu;
         }
-        else{
+        else {
+            size[pv]+=size[pu];
             par[pu]=pv;
-            rank[pv]+=rank[pu];
         }
     }
 }
