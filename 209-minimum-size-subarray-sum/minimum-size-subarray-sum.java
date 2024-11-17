b9a92b22067d0d22916799f12843844c22914595
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-17 18:32:50 +0530

    Time: 19 ms (5.09%) | Memory: 56 MB (91.78%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,18 +1,23 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int min=Integer.MAX_VALUE;
-            int sum=0;
-            int j=0;
-            for(int i=0;i<nums.length;i++){
-                sum+=nums[i];
-                // System.out.println(sum);
-                while(sum>=target){
-                    min=Math.min(min,i-j+1);
-                    sum-=nums[j++];
-                }
-
+        Deque<Integer>d=new LinkedList<>();
+        int n=nums.length;
+        long[]currsum=new long[n];
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(i==0) currsum[i]=nums[i];
+            else currsum[i]=currsum[i-1]+nums[i];
+            if(currsum[i]>=target){
+                ans=Math.min(ans,i+1);
+            }
+            while(!d.isEmpty() && currsum[i]-currsum[d.getFirst()]>=target){
+                ans=Math.min(ans,i-d.pollFirst());
+            }
+            while(!d.isEmpty() && currsum[i]<=currsum[d.getLast()]){
+                d.pollLast();
+            }
+            d.add(i);
         }
-        if(min==Integer.MAX_VALUE) return 0;
-        return min;
+        return ans==Integer.MAX_VALUE?0:ans;
     }
 }
