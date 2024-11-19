d0db74faf71204608154b61d1f01d6835a673bea
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-19 09:30:36 +0530

    Time: 74 ms (5.21%) | Memory: 56.8 MB (93.97%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,21 +1,28 @@
-class Solution 
-{
-    public long maximumSubarraySum(int[] nums, int k) 
-    {
-        Long res = 0L, cur = 0L, dup = -1L;
-        int[] pos = IntStream.range(0, 100001).map(j -> -1).toArray();
-        
-        for (int i = 0; i < nums.length; ++i)
-        {
-            cur += nums[i];
-            if (i >= k) cur -= nums[i-k];
-            
-            dup = Math.max(dup, pos[nums[i]]);
-            if (i - dup >= k) res = Math.max(res, cur);
-            
-            pos[nums[i]] = i;
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        long sum=0;
+        int j=0;
+        long max=0;
+        Map<Integer,Integer>mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            sum+=nums[i];
+            while(mp.get(nums[i])>1){
+                mp.put(nums[j],mp.getOrDefault(nums[j],0)-1);
+                if(mp.get(nums[j])==0) mp.remove(nums[j]);
+                sum-=nums[j];
+                j++;
+            }
+            if(i-j+1>k){
+                sum-=nums[j];
+                mp.put(nums[j], mp.get(nums[j]) - 1);
+                if (mp.get(nums[j]) == 0) mp.remove(nums[j]);
+                j++;
+            }
+            if(i-j+1==k){
+                max=Math.max(max,sum);
+            }
         }
-        
-        return res;
+        return max;
     }
 }
