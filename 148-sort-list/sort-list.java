23cbae9e1b92edee3ede8b08fa5d603b205c9d3a
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-20 16:59:11 +0530

    Time: 10 ms (46.17%) | Memory: 56.1 MB (74.83%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,47 +10,39 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
-			return head;
-		}
-		
-		ListNode mid = getmid (head);
-		ListNode left = sortList (head);
-		ListNode right = sortList (mid);
-        return merge(left,right);
+        if(head==null || head.next==null) return head;
+
+        ListNode mid=getmid(head);
+        ListNode l=sortList(head);
+        ListNode r=sortList(mid);
+        return merge(l,r);
     }
-    public ListNode merge(ListNode left,ListNode right){
-        
-		ListNode head = new ListNode ();
-		ListNode tail = head;
-		
-		while (left != null && right != null) {
-			if (left.val < right.val) {
-				tail.next = left;
-				left = left.next;
-			}
-			else {
-				tail.next = right;
-				right = right.next;
-			}
-			tail = tail.next;
-		}
-		
-		tail.next = left != null ? left :right;
-		return head.next;
+    public ListNode merge(ListNode l,ListNode r){
+        ListNode dummy=new ListNode(0);
+        ListNode d=dummy;
+        while(l!=null && r!=null){
+            if(l.val>r.val){
+                d.next=new ListNode(r.val);
+                r=r.next;
+            }
+            else{
+                d.next=new ListNode(l.val);
+                l=l.next;
+            }
+            d=d.next;
+        }
+        d.next=l!=null?l:r;
+        return dummy.next;
     }
     public ListNode getmid(ListNode head){
-       ListNode slow = head;
-		ListNode fast = head.next;
-		
-		while (fast != null && fast.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-		
-		head = slow.next;
-		slow.next = null;
-		return head;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        head=slow.next;
+        slow.next=null;
+        return head;
     }
-
 }
