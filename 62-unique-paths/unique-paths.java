9202814df25183caeb183cc5dc5df729905e0792
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-20 08:52:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (28.98%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,27 +1,17 @@
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-//         int [][]dp=new int[m][n];
-//         dp[0][0]=1;
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(i-1>=0) dp[i][j]=dp[i-1][j];
-//                 if(j-1>=0) dp[i][j]+=dp[i][j-1];
-//             }
-//         }
-//         return dp[m-1][n-1];
-//     }
-// }
 class Solution {
     public int uniquePaths(int m, int n) {
-        int []dp=new int[n];
-        for(int i=0;i<m;i++){
-            int []temp=new int[n];
-            temp[0]=1;
-            for(int j=1;j<n;j++){
-                temp[j]=temp[j-1]+dp[j];
+        int [][]dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
             }
-            dp=temp;
         }
-        return dp[n-1];
+        return dp[n-1][m-1];
+
     }
 }
