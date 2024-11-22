009f8dc06c284674783a286c67d6874668ed90af
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-22 09:19:23 +0530

    Time: 4 ms (75.84%) | Memory: 45.2 MB (22.10%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,21 +1,24 @@
 class Solution {
-    public int minDistance(String s, String t) {
-        int n=s.length();
-        int m=t.length();
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
         int [][]dp=new int[n][m];
         for(int []i:dp) Arrays.fill(i,-1);
-        return call(s,t,dp,n-1,m-1);
+        return call(0,0,n,m,word1,word2,dp);
     }
-    public int call(String s,String t,int [][]dp,int i,int j){
-        if(i<0 && j<0) return 0;
-        if(i<0 ) return j+1;
-        if(j<0) return i+1;
+    public int call(int i,int j,int n,int m ,String word1,String word2,int [][]dp){
+        if(i==n && j==m) return 0;
+        if(i==n) return m-j;
+        if(j==m) return n-i;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)) return dp[i][j]= call(s,t,dp,i-1,j-1);
-        int insert=1+call(s,t,dp,i,j-1);
-        int replace=1+call(s,t,dp,i-1,j-1);
-        int delete=1+call(s,t,dp,i-1,j);
-        int min=Math.min(insert,delete);
-        return dp[i][j]=Math.min(min,replace);
+        if(word1.charAt(i)==word2.charAt(j)){
+            return call(i+1,j+1,n,m,word1,word2,dp);
+        }
+        int a=1+call(i+1,j+1,n,m,word1,word2,dp);
+        int b=1+call(i,j+1,n,m,word1,word2,dp);
+        int c=1+call(i+1,j,n,m,word1,word2,dp);
+        int ans=Math.min(a,b);
+        ans=Math.min(ans,c);
+        return dp[i][j]=ans;
     }
 }
