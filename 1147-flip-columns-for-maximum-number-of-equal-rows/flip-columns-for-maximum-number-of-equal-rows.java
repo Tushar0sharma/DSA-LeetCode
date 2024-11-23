bb36f0a0ed92e45d08349574abccaba6a38cd26f
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-23 19:09:27 +0530

    Time: 18 ms (95.60%) | Memory: 59.2 MB (59.70%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
@@ -1,33 +1,15 @@
 class Solution {
-
     public int maxEqualRowsAfterFlips(int[][] matrix) {
-        int numCols = matrix[0].length;
-        int maxIdenticalRows = 0;
-
-        for (int[] currentRow : matrix) {
-            // Create array to store flipped version of current row
-            int[] flippedRow = new int[numCols];
-            int identicalRowCount = 0;
-
-            // Create flipped version of current row (0->1, 1->0)
-            for (int col = 0; col < numCols; col++) {
-                flippedRow[col] = 1 - currentRow[col];
+        Map<String,Integer>mp=new HashMap<>();
+        for(int []i:matrix){
+            StringBuilder sb=new StringBuilder();
+            for(int j=0;j<i.length;j++){
+                sb.append(i[0]==i[j]?'T':'F');
             }
-
-            // Check every row against current row and its flipped version
-            for (int[] compareRow : matrix) {
-                // If row matches either original or flipped pattern, increment counter
-                if (
-                    Arrays.equals(compareRow, currentRow) ||
-                    Arrays.equals(compareRow, flippedRow)
-                ) {
-                    identicalRowCount++;
-                }
-            }
-
-            maxIdenticalRows = Math.max(maxIdenticalRows, identicalRowCount);
+            mp.put(sb.toString(),mp.getOrDefault(sb.toString(),0)+1);
         }
-
-        return maxIdenticalRows;
+        int max=0;
+        for(int i:mp.values()) max=Math.max(max,i);
+        return max;
     }
 }
