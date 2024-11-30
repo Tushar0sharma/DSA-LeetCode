c15488f7c114a2f6adc6458a2f4b03ff73a3ef84
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-30 13:25:22 +0530

    Time: 13 ms (47.88%) | Memory: 43 MB (79.07%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.java
+++ 787-sliding-puzzle/sliding-puzzle.java
@@ -1,49 +1,37 @@
 class Solution {
     public int slidingPuzzle(int[][] board) {
-        String target = "123450";
-        String start = "";
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                start += board[i][j];
-            }
+        int [][]dir={{1,3},{0,2,4},{1,5},{0,4},{1,3,5},{2,4}};
+        String target="123450";
+        String start="";
+        for(int []i:board){
+            for(int j:i) start+=j;
         }
-        HashSet<String> visited = new HashSet<>();
-        // all the positions 0 can be swapped to
-        int[][] dirs = new int[][] { { 1, 3 }, { 0, 2, 4 },
-                { 1, 5 }, { 0, 4 }, { 1, 3, 5 }, { 2, 4 } };
-        Queue<String> queue = new LinkedList<>();
-        queue.offer(start);
-        visited.add(start);
-        int res = 0;
-        while (!queue.isEmpty()) {
-            // level count, has to use size control here, otherwise not needed
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                String cur = queue.poll();
-                if (cur.equals(target)) {
-                    return res;
-                }
-                int zero = cur.indexOf('0');
-                // swap if possible
-                for (int dir : dirs[zero]) {
-                    String next = swap(cur, zero, dir);
-                    if (visited.contains(next)) {
-                        continue;
-                    }
-                    visited.add(next);
-                    queue.offer(next);
-
+        Set<String>vis=new HashSet<>();
+        Queue<String>q=new LinkedList<>();
+        int ans=0;
+        q.add(start);
+        while(!q.isEmpty()){
+            int size=q.size();
+            while(size-->0){
+                String p=q.poll();
+                if(p.equals(target)) return ans;
+                int zeropos=p.indexOf('0');
+                for(int i:dir[zeropos]){
+                    String news=swap(p,zeropos,i);
+                    if(vis.contains(news)) continue;
+                    vis.add(news);
+                    q.add(news);
                 }
             }
-            res++;
+            ans++;
         }
         return -1;
     }
-
-    private String swap(String str, int i, int j) {
-        StringBuilder sb = new StringBuilder(str);
-        sb.setCharAt(i, str.charAt(j));
-        sb.setCharAt(j, str.charAt(i));
-        return sb.toString();
+    public String swap(String p,int i,int j){
+        char []ch=p.toCharArray();
+        char t=ch[i];
+        ch[i]=ch[j];
+        ch[j]=t;
+        return new String(ch);
     }
 }
