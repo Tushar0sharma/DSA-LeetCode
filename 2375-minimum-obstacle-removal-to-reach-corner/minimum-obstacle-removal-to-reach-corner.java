882a4a9c0cad80c4fb55d96308e4dde327b38036
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-11-30 13:04:13 +0530

    Time: 137 ms (41.26%) | Memory: 83.1 MB (15.07%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
@@ -1,51 +1,32 @@
 class Solution {
-    int n,m;
-    int []dx = new int[]{0,0,1,-1};
-    int []dy = new int[]{1,-1,0,0};
-    int [][]dp;
-    int [][]grid;
-    private boolean isValid(int i, int j, int cost) {
-        boolean valid = (Math.min(i,j)>=0 && i<n && j<m && dp[i][j]>cost+grid[i][j]);
-        if(valid) dp[i][j]=cost+grid[i][j];
-        return valid;
-    }
-    
     public int minimumObstacles(int[][] grid) {
-        this.grid = grid;
-        this.n = grid.length;
-        this.m = grid[0].length;
-        dp = new int[n][m];
-        for(int []row:dp)Arrays.fill(row,Integer.MAX_VALUE);
-        
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0,0});
-        int res = n*m+1;// At most i have to remove all the blockers.
-        
-        while(!queue.isEmpty()) {
-            int thisLevel = queue.size();
-            while(thisLevel-->0 ) {
-                int []temp = queue.remove();
-                int i=temp[0], j=temp[1];
-                if(i==n-1 && j == m-1) {
-                    res = Math.min(res, temp[2]);
-                    continue;
-                }
-                
-                if(temp[2]>dp[i][j]) {
-                    // This is invalid path because we have achived better which already might be running in the queue
-                    continue;
-                }
-                
-               for(int k=0;k<4;k++) {
-                    int newI = i+dx[k], newJ = j+dy[k];
-                   
-                   // if newi and newj is valid and which is not increasing the cost for newi and newj which already i have achieved.
-                    if(isValid(newI, newJ, temp[2])) {
-                        queue.add(new int[]{newI, newJ, dp[newI][newJ]});
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[2]-b[2]);
+        pq.add(new int[]{0,0,0});
+        int n=grid.length;
+        int m=grid[0].length;
+        int [][]vis=new int [n][m];
+        for(int []i:vis){
+            Arrays.fill(i,Integer.MAX_VALUE);
+        }
+        int [][]dir={{0,1},{1,0},{-1,0},{0,-1}};
+        while(!pq.isEmpty()){
+            int []p=pq.poll();
+            int r=p[0];
+            int c=p[1];
+            int t=p[2];
+            if(r==n-1 && c==m-1) return t;
+            for(int []i:dir){
+                int nr=r+i[0];
+                int nc=c+i[1];
+                if(nr>=0 && nc>=0 && nr<n && nc<m){
+                    int nt=t+grid[nr][nc];
+                    if(vis[nr][nc]>nt){
+                        vis[nr][nc]=nt;
+                        pq.add(new int[]{nr,nc,nt});
                     }
                 }
             }
         }
-        return res;
+        return -1;
     }
 }
