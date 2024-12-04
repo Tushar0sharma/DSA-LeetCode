0743c507529a6a4f4bbe5b548033c9f0eebe8b8a
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-04 14:42:01 +0530

    Time: 5 ms (25.40%) | Memory: 51.1 MB (84.83%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -2,45 +2,22 @@ class Solution {
     public int countServers(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int []par=new int[n+m+1];
-        int []size=new int[n+m+1];
-        for(int i=0;i<n+m+1;i++){
-            par[i]=i;
-        }
+        Map<Integer,Integer>r=new HashMap<>();
+        Map<Integer,Integer>c=new HashMap<>();
+        int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    int row=i;
-                    int col=j+n;
-                    union(row,col,par,size);
+                    r.put(i,r.getOrDefault(i,0)+1);
+                    c.put(j,c.getOrDefault(j,0)+1);
                 }
             }
         }
-        System.out.println(Arrays.toString(size));
-        int cnt=0;
-        for(int i=0;i<n+m+1;i++){
-            if(size[i]>1 && par[i]==i) cnt+=size[i];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 &&(r.get(i)>1 || c.get(j)>1)) cnt++;
+            }
         }
         return cnt;
-    }
-    public int find(int p,int []par){
-        if(p==par[p]) return p;
-        return par[p]=find(par[p],par);
-    }
-    public void union(int u,int v,int []par,int []size){
-        int pu=find(u,par);
-        int pv=find(v,par);
-        if(pu==pv){
-            size[pu]++;
-            return;
-        }
-        if(size[pu]>size[pv]){
-            size[pu]+=size[pv]+1;
-            par[pv]=pu;
-        }
-        else{
-            size[pv]+=size[pu]+1;
-            par[pu]=pv;
-        }
     }
 }
