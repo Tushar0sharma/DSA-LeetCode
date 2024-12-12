b7a4e92a024df71f2b2b7348ab2d2ee42ba6281d
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-13 01:57:56 +0530

    Time: 8 ms (99.28%) | Memory: 64.8 MB (7.59%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -2,18 +2,19 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
         int []ans=new int[n-k+1];
-        Deque<Integer>dq=new ArrayDeque<>();
-        for(int i=0;i<n;i++){
-            while(!dq.isEmpty() && i-k>=dq.peekFirst()){
-                dq.pollFirst();
-            }
-            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
-                dq.pollLast();
-            }
-            dq.add(i);
-            if(i-k+1>=0){
-                ans[i-k+1]=nums[dq.peekFirst()];
-            }
+        
+        int []lmax=new int[n];
+        int []rmax=new int[n];
+        lmax[0]=nums[0];
+        rmax[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            lmax[i]=(i%k==0)?nums[i]:Math.max(nums[i],lmax[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            rmax[i]=(i%k==0)?nums[i]:Math.max(nums[i],rmax[i+1]);
+        }
+        for(int i=0,j=i+k-1;j<n;j++,i++){
+            ans[i]=Math.max(rmax[i],lmax[j]);
         }
         return ans;
     }
