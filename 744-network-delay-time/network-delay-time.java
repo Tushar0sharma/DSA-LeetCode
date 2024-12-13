bba1adc59f15b3a3554d37db067e075bf9b6e7e4
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-13 16:15:16 +0530

    Time: 10 ms (86.05%) | Memory: 48.9 MB (28.08%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,32 +1,29 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         List<List<int[]>>ll=new ArrayList<>();
-        int []dist=new int[n+1];
-        for(int i=0;i<=n;i++){
-            ll.add(new ArrayList<>());
-        }
+        for(int i=0;i<=n;i++) ll.add(new ArrayList<>());
         for(int []i:times){
             ll.get(i[0]).add(new int[]{i[1],i[2]});
         }
         PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
-        pq.add(new int[]{k,0});
+        pq.add(new int []{k,0});
+        int []dist=new int[n+1];
         Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k]=0;
+        int max=0;
         while(!pq.isEmpty()){
-            int []p=pq.poll();
-            int node=p[0];
-            int cost=p[1];
+            int[] q=pq.poll();
+            int node=q[0];
+            int dis=q[1];
             for(int []i:ll.get(node)){
-                int nt=i[1]+cost;
-                if(dist[i[0]]>nt) {
-                    pq.add(new int[]{i[0],nt});
-                    dist[i[0]]=nt;
+                if(dis+i[1]<dist[i[0]]){
+                    dist[i[0]]=i[1]+dis;
+                    pq.add(new int[]{i[0],dist[i[0]]});
                 }
             }
         }
-        int max=0;
-        for(int i=1;i<=n;i++) {
-            max=Math.max(max,dist[i]);
+        for(int i=1;i<n+1;i++) {
+            if(i==k) continue;
+            max=Math.max(dist[i],max);
             if(dist[i]==Integer.MAX_VALUE) return -1;
         }
         return max;
