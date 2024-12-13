e1ddd455f6586ad427cf746ac516b762e296f195
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-13 23:43:35 +0530

    Time: 19 ms (59.04%) | Memory: 60.4 MB (8.57%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int furthestBuilding(int[] height, int bricks, int ladders) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0;i<height.length-1;i++){
-            if(height[i]>=height[i+1]) continue;
-            bricks-=height[i+1]-height[i];
-            pq.add(height[i+1]-height[i]);
-
+    public int furthestBuilding(int[] h, int bricks, int ladders) {
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        int n=h.length;
+        for(int i=0;i<n-1;i++){
+            if(h[i]>=h[i+1]) continue;
+            bricks-=h[i+1]-h[i];
+            pq.add(h[i+1]-h[i]);
             if(bricks<0){
                 bricks+=pq.poll();
                 if(ladders>0) ladders--;
                 else return i;
             }
         }
-      return height.length-1;  
-        
+        return n-1;
     }
 }
