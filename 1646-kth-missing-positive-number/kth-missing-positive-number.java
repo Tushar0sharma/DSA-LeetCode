eb9689c2323776de6eeacfcc0a7ef8948ebf1e6a
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-20 15:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (7.93%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,45 +1,16 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int[] allArray = new int[arr[arr.length-1]+k];
-        for(int i = 0; i<arr[arr.length-1]+k;i++){
-            allArray[i]=i+1;
-        }
-        for(int j=0; j<arr.length;j++){
-            // int index = binarySearch(allArray, arr[j]);
-            allArray[arr[j]-1]=-1;
-        }
-        int count=0;
-        
-        for(int l: allArray){
-            if(l== -1){
-                
-                continue;
-            }else{
-                count++;
+        int l=0;
+        int r=arr.length-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(arr[m]-m-1<k){
+                l=m+1;
             }
-            if( count == k){
-                return allArray[l-1];
-            }
-
-        }
-        return -1;
-    }
-
-    public int binarySearch(int[] arr, int key){
-        int low = 0;
-        int high = arr.length-1;
-
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(arr[mid] == key){
-                return mid;
-            }
-            if(arr[mid]> key){
-                high = mid-1;
-            }else{
-                low=mid+1;
+            else{
+                r=m-1;
             }
         }
-        return -1;
+        return l+k;
     }
 }
