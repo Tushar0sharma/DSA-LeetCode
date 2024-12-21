2c76b4d33ab6d787ad4e760d3f7868fdddf9cc96
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-21 18:48:14 +0530

    Time: 111 ms (35.53%) | Memory: 97.5 MB (57.86%) - LeetSync

--- 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.java
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.java
@@ -15,43 +15,53 @@
  */
 class Solution {
     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
-        List<List<Integer>>ll=new ArrayList<>();
         List<Integer>l=new ArrayList<>();
         call(l,root);
-        int n=l.size();
+        List<List<Integer>>ll=new ArrayList<>();
         for(int q:queries){
             List<Integer>a=new ArrayList<>();
-            int i=0;
-            int j=n-1;
-            int lb=Integer.MIN_VALUE;
-            int hb=Integer.MAX_VALUE;
-            while(i<=j){
-                int mid=(i+j)/2;
-                if(l.get(mid)>q){
-                    j=mid-1;
-                    hb=Math.min(hb,l.get(mid));
-                }
-                else if(l.get(mid)<q){
-                    i=mid+1;
-                    lb=Math.max(lb,l.get(mid));
-                }
-                else{
-                    lb=q;
-                    hb=q;
-                    break;
-                }
-            }
-            if(hb==Integer.MAX_VALUE) hb=-1;
-            if(lb==Integer.MIN_VALUE) lb=-1;
-            
-            a.add(lb);
-            a.add(hb);
-            ll.add(new ArrayList<>(a));
+            int min=lower(q,l);
+            int max=upper(q,l);
+            a.add(min);
+            a.add(max);
+            ll.add(a);
         }
         return ll;
     }
+    public int upper(int target,List<Integer>ll){
+        int l=0;
+        int r=ll.size()-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(ll.get(mid)<target){
+                l=mid+1;
+            }
+            else{
+                ans=ll.get(mid);
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+    public int lower(int target,List<Integer>ll){
+        int l=0;
+        int r=ll.size()-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(ll.get(mid)<=target){
+                ans=ll.get(mid);
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
     public void call(List<Integer>l,TreeNode root){
-        if(root==null) return ;
+        if(root==null) return;
         call(l,root.left);
         l.add(root.val);
         call(l,root.right);
