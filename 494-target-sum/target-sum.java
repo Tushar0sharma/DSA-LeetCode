914a4f846f95fb1df8f445976073d8ff53256b15
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-26 15:12:41 +0530

    Time: 8 ms (69.73%) | Memory: 44.6 MB (35.83%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,20 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return call(0,target,0,nums);
+        int offset=0;
+        for(int i:nums) offset+=i;
+        int [][]dp=new int[nums.length+1][2*offset+1];
+        for(int i[]:dp) Arrays.fill(i,-1);
+        return call(0,target,0,nums,dp,offset);
     }
-    public int call(int i,int t,int sum,int []nums){
+    public int call(int i,int t,int sum,int []nums,int [][]dp,int a){
         if(i==nums.length){
             if(t==sum) return 1;
             else return 0;
         } 
+        if(dp[i][a+sum]!=-1) return dp[i][a+sum];
         int ans=0;
-        ans+=call(i+1,t,sum-nums[i],nums);
-        ans+=call(i+1,t,sum+nums[i],nums);
-        return ans;
+        ans+=call(i+1,t,sum-nums[i],nums,dp,a);
+        ans+=call(i+1,t,sum+nums[i],nums,dp,a);
+        return dp[i][sum+a]= ans;
     }
 }
