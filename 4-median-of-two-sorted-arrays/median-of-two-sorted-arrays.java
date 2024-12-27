074269d9247a1505461fe0d6ccb7a048143918a9
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-28 00:31:57 +0530

    Time: 94 ms (5.06%) | Memory: 47 MB (7.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,28 +3,54 @@ class Solution {
         int n=nums1.length;
         int m=nums2.length;
         int k=n+m;
-        int []arr=new int[k];
+        int cnt1=0;
+        int cnt2=0;
         int a=0;
         int i=0,j=0;
+        System.out.println((k/2)+" "+(k/2-1));
         while(i<n && j<m){
             if(nums1[i]<nums2[j]){
-                arr[a++]=nums1[i++];
+                if(a==(k/2-1)) cnt1=nums1[i];
+                if(a==(k/2)) {
+                    cnt2=nums1[i];
+                }
+                i++;
+                a++;
             }
             else{
-                arr[a++]=nums2[j++];
+                if(a==(k/2-1)) cnt1=nums2[j];
+                if(a==(k/2)){
+                    cnt2=nums2[j];
+                }
+                j++;
+                a++;
             }
+            System.out.println(cnt1+" "+cnt2);
         }
         while(i<n){
-            arr[a++]=nums1[i++];
+            if(a==(k/2-1)) cnt1=nums1[i];
+            if(a==(k/2)){
+                cnt2=nums1[i];
+                break;
+            }
+            i++;
+            a++;
         }
         while(j<m){
-            arr[a++]=nums2[j++];
+            if(a==(k/2-1)) cnt1=nums2[j];
+            if(a==(k/2)){
+                cnt2=nums2[j];
+                break;
+            }
+            j++;
+            a++;
         }
+        System.out.println(cnt1+" "+cnt2);
         if(k%2!=0){
-            return arr[k/2];
+            return cnt2;
         }
         else{
-            return (double)(arr[k/2]+arr[(k/2)-1])/2.0;
+            return (cnt1+cnt2)/2.0;
         }
     }
 }
