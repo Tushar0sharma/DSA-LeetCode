1549c347e9600c1c6419aff072070c66e612a753
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-29 02:12:02 +0530

    Time: 17 ms (93.49%) | Memory: 57.8 MB (49.74%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,27 +1,14 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        int n=nums.length;
-    //     HashMap<Integer,Integer>ans=new HashMap<Integer,Integer>();
-    //    for(int i=0;i<n;i++){
-    //        if(ans.containsKey(nums[i])){
-    //            if(i-ans.get(nums[i]) <=k) return true;
-    //        }
-    //        ans.put(nums[i],i);
-    //    }
-    //    return false;
-     if(nums == null || nums.length < 2 || k == 0)
-            return false;
-    int j=0;
-      HashSet<Integer>ans=new HashSet<Integer>();
-      for(int i=0;i<n;i++){
-          if(!ans.add(nums[i])){
-             return true;
-          }
-          if(ans.size()>=k+1){
-              ans.remove(nums[j++]);
-          }
-      }
-      return false;
-        
+        int i=0,j=0;
+        Set<Integer>s=new HashSet<>();
+        while(j<nums.length){
+            if(j-i>k){
+                s.remove(nums[i++]);
+            }
+            if(!s.add(nums[j])) return true;
+            j++;
+        }
+        return false;
     }
 }
