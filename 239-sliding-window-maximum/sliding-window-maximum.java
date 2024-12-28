9ee7c935084544774d1185ba2c64c2f1ff291434
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-28 21:56:41 +0530

    Time: 7 ms (99.41%) | Memory: 65 MB (5.93%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -2,19 +2,18 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
         int []ans=new int[n-k+1];
-        
         int []lmax=new int[n];
         int []rmax=new int[n];
         lmax[0]=nums[0];
         rmax[n-1]=nums[n-1];
         for(int i=1;i<n;i++){
-            lmax[i]=(i%k==0)?nums[i]:Math.max(nums[i],lmax[i-1]);
+            lmax[i]=(i%k==0)?nums[i]:Math.max(lmax[i-1],nums[i]);
         }
         for(int i=n-2;i>=0;i--){
-            rmax[i]=(i%k==0)?nums[i]:Math.max(nums[i],rmax[i+1]);
+            rmax[i]=(i%k==0)?nums[i]:Math.max(rmax[i+1],nums[i]);
         }
-        for(int i=0,j=i+k-1;j<n;j++,i++){
-            ans[i]=Math.max(rmax[i],lmax[j]);
+        for(int i=0,j=i+k-1;j<n;i++,j++){
+            ans[i]=Math.max(lmax[j],rmax[i]);
         }
         return ans;
     }
