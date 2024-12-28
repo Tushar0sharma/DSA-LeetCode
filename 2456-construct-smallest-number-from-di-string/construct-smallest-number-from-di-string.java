fa181fad2033c689c60febc677162b00a10b2863
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-29 03:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (58.36%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,29 +1,14 @@
 class Solution {
     public String smallestNumber(String pattern) {
         StringBuilder s=new StringBuilder();
-
-        int x=1;
-        for(int i=0;i<pattern.length();i++){
-            if(pattern.charAt(i)=='D'){
-                int dcount=0;
-                while(i<pattern.length() && pattern.charAt(i)=='D'){
-                    dcount++;
-                    i++;
-                }
-                int temp=dcount;
-                while(dcount>0){
-                    s.append(x+dcount);
-                    dcount--;
-                }
-                s.append(x);
-                x+=temp;
+        StringBuilder stack=new StringBuilder();
+        for(int i=0;i<=pattern.length();i++){
+            stack.append((char)('1'+i));
+            if(pattern.length()==i || pattern.charAt(i)=='I'){
+                s.append(stack.reverse());
+                stack=new StringBuilder();
             }
-            else{
-                s.append(x);
-            }
-            x++;
         }
-        if(pattern.charAt(pattern.length()-1)=='I') s.append(x);
         return s.toString();
     }
 }
