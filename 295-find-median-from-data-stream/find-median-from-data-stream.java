48969cfe2d7cdf7ca5e702036a4a1a1263821faf
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-28 23:44:01 +0530

    Time: 135 ms (15.61%) | Memory: 63.9 MB (26.45%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,24 +1,22 @@
 class MedianFinder {
-
-    PriorityQueue<Integer>pq1;
-    PriorityQueue<Integer>pq2;
+    PriorityQueue<Integer>min=new PriorityQueue<>();
+    PriorityQueue<Integer>max=new PriorityQueue<>(Collections.reverseOrder());
 
     public MedianFinder() {
-        pq1=new PriorityQueue<>();
-        pq2=new PriorityQueue<>(Collections.reverseOrder());
+        
     }
     
     public void addNum(int num) {
-        pq2.add(num);
-        pq1.add(pq2.poll());
-        if(pq1.size()>pq2.size()){
-            pq2.add(pq1.poll());
-        }        
+        max.add(num);
+        min.add(max.poll());
+        if(min.size()>max.size()){
+            max.add(min.poll());
+        }
     }
     
     public double findMedian() {
-        if(pq1.size()<pq2.size()) return pq2.peek();
-        return (pq1.peek()+pq2.peek())/2.0d;
+        if(max.size()!=min.size()) return (double)max.peek();
+        return (min.peek()+max.peek())/2.0;
     }
 }
 
