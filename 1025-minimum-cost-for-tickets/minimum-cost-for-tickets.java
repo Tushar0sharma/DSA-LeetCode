5fd213c878c0b5ab5c9ee19762a6de795641fdf5
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-12-31 17:07:39 +0530

    Time: 19 ms (5.21%) | Memory: 50.9 MB (5.09%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -3,9 +3,9 @@ class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         dp=new int[366][1000];
         for(int []i:dp) Arrays.fill(i,-1);
-        return call(0,0,0,days,costs);
+        return call(0,0,days,costs);
     }
-    public int call(int i,int d,int cnt,int []day,int []cost){
+    public int call(int i,int d,int []day,int []cost){
         if(i==day.length) return 0;
         int a=0;
         int b=0;
@@ -13,13 +13,13 @@ class Solution {
         int min=0;
         if(dp[i][d]!=-1) return dp[i][d];
         if(d<day[i]){
-            a=cost[0]+call(i+1,day[i],cnt+cost[0],day,cost);
-            b=cost[1]+call(i+1,day[i]+6,cnt+cost[1],day,cost);
-            c=cost[2]+call(i+1,day[i]+29,cnt+cost[2],day,cost);
+            a=cost[0]+call(i+1,day[i],day,cost);
+            b=cost[1]+call(i+1,day[i]+6,day,cost);
+            c=cost[2]+call(i+1,day[i]+29,day,cost);
             min=Math.min(a,Math.min(b,c));
         }
         else{
-            min=call(i+1,d,cnt,day,cost);
+            min=call(i+1,d,day,cost);
         }
         return dp[i][d]=min;
     }
