622cac51d38bfb4a51555cabc6e361b55aab9a45
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-05 16:17:18 +0530

    Time: 100 ms (11.15%) | Memory: 46.3 MB (5.35%) - LeetSync

--- 3591-shift-distance-between-two-strings/shift-distance-between-two-strings.java
+++ 3591-shift-distance-between-two-strings/shift-distance-between-two-strings.java
@@ -1,50 +1,15 @@
 class Solution {
-    Map<Character,Map<Character,Integer>>mp;
-    Map<String,Long>cache;
     public long shiftDistance(String s, String t, int[] nextCost, int[] previousCost) {
-        mp=new HashMap<>();
-        cache=new HashMap<>();
-
-        for(int i=0;i<26;i++){
-            mp.put((char)('a'+i),new HashMap<>());
-        }
-        for(int i=0;i<25;i++){
-            mp.get((char)('a'+i)).put((char)('a'+i+1),nextCost[i]);
-        }
-        mp.get('z').put('a',nextCost[25]);
-        mp.get('a').put('z',previousCost[0]);
-        for(int i=1;i<26;i++)
-        {
-            mp.get((char)('a'+i)).put((char)('a'+i-1),previousCost[i]);
-        }
         long ans=0;
         for(int i=0;i<s.length();i++){
-            ans+=call(s.charAt(i),t.charAt(i));
+            int ss=s.charAt(i)-'a';
+            int tt=t.charAt(i)-'a';
+            if(ss==tt) continue;
+            long prev=0,next=0;
+            for(int j=ss;j!=tt;j=(j+1)%26) next+=nextCost[j];
+            for(int j=ss;j!=tt;j=(j-1+26)%26) prev+=previousCost[j];
+            ans+=Math.min(prev,next);
         }
         return ans;
-    }
-    public long call(char c,char t){
-        if(c==t) return 0;
-        if(cache.containsKey(c+","+t)){
-            return cache.get(c+","+t);
-        }
-        PriorityQueue<long[]>pq=new PriorityQueue<>((a,b)->Long.compare(a[0],b[0]));
-        Set<Character>vis=new HashSet<>();
-        pq.add(new long[]{0,c});
-        while(!pq.isEmpty()){
-            long []p=pq.poll();
-            char node=(char)p[1];
-            if(node==t){ 
-                cache.put(c+","+t,p[0]);
-                return p[0];
-            }
-            if(vis.contains(node)) continue;
-            vis.add(node);
-            for(char m:mp.get(node).keySet()){
-                int cost=mp.get(node).get(m);
-                pq.add(new long[]{p[0]+cost,m});
-            }
-        }
-        return -1;
     }
 }
