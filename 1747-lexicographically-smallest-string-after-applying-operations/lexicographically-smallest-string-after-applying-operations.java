9bc7c086f741dd0389960a47daab5fb59d7eb519
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-06 22:16:23 +0530

    Time: 63 ms (93.58%) | Memory: 51.3 MB (42.78%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -1,21 +1,21 @@
 class Solution {
+    Set<String>seen=new HashSet<>();
+    String min;
     public String findLexSmallestString(String s, int a, int b) {
-        Queue<String>q=new LinkedList<>();
-        Set<String>seen=new HashSet<>();
-        q.add(s);
-        String min=s;
-        while(!q.isEmpty()){
-            String p=q.poll();
-            if(p.compareTo(min)<0) min=p;
-            String rev=p.substring(p.length()-b)+p.substring(0,s.length()-b);
-            if(seen.add(rev)) q.add(rev);
-            char []ch=p.toCharArray();
-            for(int i=1;i<s.length();i+=2){
-                ch[i]=(char)((ch[i]-'0'+a)%10+'0');
-            }
-            String chh=new String(ch);
-            if(seen.add(chh)) q.add(chh);
-        }
+        min=s;
+        dfs(s,a,b);
         return min;
+    }
+    public void dfs(String s,int a,int b){
+        if(seen.contains(s)) return;
+        if(min.compareTo(s)>0) min=s;
+        seen.add(s);
+        String rev=s.substring(s.length()-b)+s.substring(0,s.length()-b);
+        dfs(rev,a,b);
+        char []ch=s.toCharArray();
+        for(int i=1;i<s.length();i+=2){
+            ch[i]=(char)((ch[i]-'0'+a)%10+'0');
+        }
+        dfs(new String(ch),a,b);
     }
 }
