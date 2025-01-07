7dd912f09142747c5de828c89c3cdb55869b78a8
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-07 18:10:42 +0530

    Time: 10 ms (6.83%) | Memory: 45.1 MB (6.00%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,14 +1,57 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        Set<String>ans=new HashSet<>();
+        List<String>ans=new ArrayList<>();
         int n=words.length;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(words[i].contains(words[j]) && j!=i ){
-                    ans.add(words[j]);
+                if(kmp(words[j],words[i]) && i!=j) {ans.add(words[i]);break;}
+            }
+        }
+        return ans;
+    }
+    public boolean kmp(String str,String pat){
+        int n=str.length();
+        int m=pat.length();
+        int i=0,j=0;
+        int []lps=call(pat);
+        while(i<n){
+            if(pat.charAt(j)==str.charAt(i)){
+                i++;
+                j++;
+                if(j==m) return true;
+            }
+            else{
+                if(j>0){
+                    j=lps[j-1];
+                }else{
+                    i++;
                 }
             }
         }
-        return new ArrayList<>(ans);
+        return false;
+    }
+    public int[] call(String pat){
+        int len=0;
+        int n=pat.length();
+        int[]lps=new int[n];
+        int i=1;
+        lps[0]=0;
+        while(i<n){
+            if(pat.charAt(i)==pat.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len>0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 }
