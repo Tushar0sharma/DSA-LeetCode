9a53b3abbfe988e19c07c51c9357fead643fd0a9
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-07 19:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (56.29%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,17 +1,44 @@
 class Solution {
     public int strStr(String h, String n) {
+        return call(h,n);
+    }
+    public int call (String s,String pat){
         int i=0;
         int j=0;
-        while(j<n.length() && i<h.length()){
-            int temp=i;
-            while(j<n.length() && i<h.length() && n.charAt(j)==h.charAt(i)){
+        int n=s.length();
+        int m=pat.length();
+        int []lps=calllps(pat);
+        while(i<n){
+            if(s.charAt(i)==pat.charAt(j)){
                 i++;
                 j++;
+                if(j==m) return i-m;
             }
-            if(j==n.length()) return temp;
-            i=temp+1;
-            j=0;
+            else{
+                if(j>0) j=lps[j-1];
+                else {
+                    i++;
+                }
+            }
         }
         return -1;
+    }
+    public int [] calllps(String pat){
+        int n=pat.length();
+        int []lps=new int[n];
+        lps[0]=0;
+        int i=1;
+        int len=0;
+        while(i<n){
+            if(pat.charAt(i)==pat.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }else{
+                if(len>0) len=lps[len-1];
+                else i++;
+            }
+        }
+        return lps;
     }
 }
