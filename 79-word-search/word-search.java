85794ceccca3bce4d3d06df620babe19525e4f4c
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-09 19:28:26 +0530

    Time: 145 ms (57.67%) | Memory: 41.8 MB (43.90%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,26 +1,20 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n=board.length;
-        int m=board[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(call(board,n,m,i,j,word,0)) return true;
+                    if(call(i,j,word,board,new boolean[board.length][board[0].length],0)) return true;
                 }
             }
         }
         return false;
     }
-    public boolean call(char[][]board,int n,int m,int i,int j,String w,int idx){
-        if(idx==w.length()) return true;
-        if(i>=n || j>=m || i<0 || j<0 || board[i][j]!=w.charAt(idx) ) return false;
-        int []r={1,-1,0,0};
-        int []c={0,0,-1,1};
-        board[i][j]='#';
-        for(int k=0;k<4;k++){
-            if(call(board,n,m,i+r[k],j+c[k],w,idx+1)) return true;
-        }
-        board[i][j]=w.charAt(idx);
-        return false;
+    public boolean call(int i,int j,String w,char[][]ch,boolean [][]vis,int k){
+        if(k==w.length()) return true;
+        if(i<0  || j<0 || i>=ch.length ||j>=ch[0].length  || w.charAt(k)!=ch[i][j] || vis[i][j]) return false;
+        vis[i][j]=true;
+        boolean b= call(i+1,j,w,ch,vis,k+1)||call(i-1,j,w,ch,vis,k+1)||call(i,j-1,w,ch,vis,k+1)||call(i,j+1,w,ch,vis,k+1);
+        vis[i][j]=false;
+        return b;
     }
 }
