663b9666ad662ad6f44ec2db16f8c992c4edb0e2
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-09 23:51:32 +0530

    Time: 9 ms (6.21%) | Memory: 45.9 MB (5.10%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,9 +1,38 @@
+class trie{
+    int cnt;
+    trie[]arr;
+    trie(){
+        cnt=0;
+        arr=new trie[26];
+        for(int i=0;i<26;i++) arr[i]=null;
+    }
+}
 class Solution {
-    public int prefixCount(String[] words, String pref) {
-        int a=0;
-        for(String s:words){
-            if(s.startsWith(pref)) a++;
+    public void call(trie root,String s){
+        trie craw=root;
+        for(int i=0;i<s.length();i++){
+            int idx=s.charAt(i)-'a';
+            if(craw.arr[idx]==null){
+                craw.arr[idx]=new trie();
+            }
+            craw=craw.arr[idx];
+            craw.cnt++;
         }
-        return a;
+    }
+    public int find(String s,trie root){
+        trie c=root;
+        for(char ch:s.toCharArray()){
+            int idx=ch-'a';
+            if(c.arr[idx]==null) return 0;
+            c=c.arr[idx];
+        }
+        return c.cnt;
+    }
+    public int prefixCount(String[] words, String pref) {
+        trie root=new trie();
+        for(String s:words){
+            call(root,s);
+        }
+        return find(pref,root);
     }
 }
