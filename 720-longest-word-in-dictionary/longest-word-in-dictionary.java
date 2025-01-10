56dff84971c616df59dc9fa1455b8b3d1f847277
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-10 22:11:14 +0530

    Time: 7 ms (74.76%) | Memory: 44.9 MB (69.78%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -1,20 +1,38 @@
+class trie{
+    trie[]arr;
+    String word;
+    trie(){
+        this.arr=new trie[26];
+        for(int i=0;i<26;i++) this.arr[i]=null;
+        this.word="";
+    }
+}
 class Solution {
+    String res="";
+    public void call(trie root,String s){
+        trie node=root;
+        for(char ch:s.toCharArray()){
+            int idx=ch-'a';
+            if(node.arr[idx]==null) node.arr[idx]=new trie();
+            node =node.arr[idx];
+        }
+        node.word=s;
+    }
+    public void dfs(trie root){
+        if(!root.word.isEmpty()){
+            if(res.length()<root.word.length()) res=root.word;
+            else if(res.length()==root.word.length() && res.compareTo(root.word)>0) res=root.word;
+        }
+        for(trie t:root.arr){
+            if(t!=null && !t.word.isEmpty()) dfs(t);
+        }
+    }
     public String longestWord(String[] words) {
-        Set<String>s=new HashSet<>();
-        for(String str:words) s.add(str);
-        String res="";
-
-        for(String st:words){
-            boolean b=true;
-            for(int i=1;i<=st.length();i++){
-                String ss=st.substring(0,i);
-                if(!s.contains(ss)) b=false;
-            }
-            if(b){
-                if(res.length()<st.length()) res=st;
-                else if(res.length()==st.length() && res.compareTo(st)>0) res=st;
-            }
-        } 
+        trie root=new trie();
+        for(String s:words){
+            call(root,s);
+        }
+        dfs(root);
         return res;
     }
 }
