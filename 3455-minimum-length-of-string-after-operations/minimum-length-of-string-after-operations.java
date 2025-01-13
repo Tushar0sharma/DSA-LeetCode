8da7eb243eaac2ed9043f8beaa1f48d44621c210
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-13 09:11:37 +0530

    Time: 28 ms (49.67%) | Memory: 46 MB (89.40%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,14 +1,15 @@
 class Solution {
     public int minimumLength(String s) {
         int []arr=new int[26];
-        int a=0;
+        int ans=0;
         for(char ch:s.toCharArray()){
             arr[ch-'a']++;
             if(arr[ch-'a']==3){
-                a+=2;
+                ans-=2;
                 arr[ch-'a']=1;
             }
+            ans++;
         }
-        return s.length()-a;
+        return ans;
     }
 }
