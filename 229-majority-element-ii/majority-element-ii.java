375acf52b35ba49d887abde92ed0bc3ea5b2f288
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-18 15:16:03 +0530

    Time: 2 ms (99.24%) | Memory: 48.3 MB (38.84%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer>mp=new HashMap<>();
-
-        for(int num:nums){
-            mp.put(num,mp.getOrDefault(num,0)+1);
-        }
-
         List<Integer>l=new ArrayList<>();
-        int threshold=nums.length/3;
-
-        for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-            int element=entry.getKey();
-            int cnt=entry.getValue();
-
-            if(cnt>threshold){
-                l.add(element);
+        if(nums.length==0) return l;
+        int cnt1=0,cnt2=0,num1=nums[0],num2=nums[0];
+        for(int i:nums){
+            if(i==num1){
+                cnt1++;
+            }
+            else if(i==num2) {cnt2++;}
+            else if(cnt1==0){
+                cnt1++;
+                num1=i;
+            }
+            else if(cnt2==0){
+                cnt2++;
+                num2=i;
+            }
+            else {
+                cnt1--;
+                cnt2--;
             }
         }
-
+        cnt1=0;cnt2=0;
+        for(int i:nums){
+            if(i==num1) cnt1++;
+            else if(i==num2) cnt2++;
+        }
+        if(cnt1>nums.length/3) l.add(num1);        
+        if(cnt2>nums.length/3) l.add(num2);
         return l;
     }
 }
