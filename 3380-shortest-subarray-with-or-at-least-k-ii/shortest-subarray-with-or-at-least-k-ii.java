5d1946152a3c204ae374ebca19cddaa9ff06a510
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-18 13:10:55 +0530

    Time: 70 ms (19.19%) | Memory: 70.3 MB (20.43%) - LeetSync

--- 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.java
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.java
@@ -1,33 +1,36 @@
 class Solution {
     public int minimumSubarrayLength(int[] nums, int k) {
-        int n=nums.length;
-        int min=Integer.MAX_VALUE;
-        int []bit=new int[32];
         int l=0;
-        for(int i=0;i<n;i++){
-            update(bit,nums[i],true);
-            while(check(bit,k) && l<=i){
-                min=Math.min(i-l+1,min);
-                update(bit,nums[l],false);
+        int r=0;
+        int min=Integer.MAX_VALUE;
+        int n=nums.length;
+        int []bit=new int[32];
+
+        while(r<n){
+            update(bit,nums[r],1);
+            while(call(bit,k)&& l<=r){
+                min= Math.min(min,r-l+1);
+                update(bit,nums[l],-1);
                 l++;
             }
+            r++;
         }
         return min==Integer.MAX_VALUE?-1:min;
     }
-    public boolean check(int []bit,int k){
-        int num=0;
+    public void update(int[]bit,int num,int d){
         for(int i=0;i<32;i++){
-            int b=(bit[i]>0)?1:0;
-            num+=b*Math.pow(2,i);
+            if(((1<<i)&num)!=0){
+                bit[i]+=d;
+            }
         }
-        return num>=k;
     }
-    public void update(int []bit,int num,boolean b){
-        for(int i=0;i<32;i++){
-            if(num<=0) break;
-            int k=((b)?1:-1)*(num%2);
-            bit[i]=bit[i]+k;
-            num/=2;
+    public boolean call(int[]bit,int k){
+        int a=0;
+        for(int i=0;i<32;i++) {
+            if(bit[i]>0){
+                a+=(1<<i);
+            }
         }
-    } 
+        return a>=k;
+    }
 }
