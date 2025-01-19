15ab41d98ce6ec1fff5fd96038ff390306ef3711
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-19 11:56:30 +0530

    Time: 1 ms (85.76%) | Memory: 42.6 MB (8.09%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,21 +1,27 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>>ll=new ArrayList<>();
-        for(int i=1;i<=numRows;i++){
-            List<Integer>l=new ArrayList<>();
-            for(int j=1;j<=i;j++){
-                l.add(findncr(i-1,j-1));
+        List<Integer>l=new ArrayList<>();
+        l.add(1);
+        ll.add(l);
+        for(int i=1;i<numRows;i++){
+            List<Integer>curr=new ArrayList<>();
+            List<Integer>prev=ll.get(i-1);
+            curr.add(1);
+            for(int j=1;j<i;j++){
+                curr.add(prev.get(j-1)+prev.get(j));
             }
-            ll.add(new ArrayList<>(l));
+            curr.add(1);
+            ll.add(new ArrayList<>(curr));
         }
         return ll;
     }
-    public int findncr(int n,int r){
-        int res=1;
-        for(int i=0;i<r;i++){
-            res*=(n-i);
-            res/=(i+1);
-        }
-        return res;
-    }
+    // public int findncr(int n,int r){
+    //     int res=1;
+    //     for(int i=0;i<r;i++){
+    //         res*=(n-i);
+    //         res/=(i+1);
+    //     }
+    //     return res;
+    // }
 }
