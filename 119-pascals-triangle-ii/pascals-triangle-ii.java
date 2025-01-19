c134fee9e204cbf252513ff6cba34143b90b4903
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-19 12:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (28.66%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,20 +1,30 @@
 class Solution {
     public List<Integer> getRow(int n) {
-        List<List<Integer>>ll=new ArrayList<>();
-        for(int i=1;i<=n+1;i++){
-            ll.add(generaterow(i));
+        List<Integer>ll=new ArrayList<>();
+        // for(int i=1;i<=n+1;i++){
+        //     ll.add(generaterow(i));
+        // }
+        int [][]arr=new int[n+1][n+1];
+        for(int i=0;i<=n;i++) arr[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++)
+            {
+                arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+            }
+        }for (int i = 0; i <= n; i++) {
+            ll.add(arr[n][i]);
         }
-        return ll.get(ll.size()-1);
+        return ll;
     }
-    public List<Integer> generaterow(int i){
-        long ans=1;
-        List<Integer>l=new ArrayList<>();
-        l.add(1);
-        for(int j=1;j<i;j++){
-            ans*=(i-j);
-            ans/=j;
-            l.add((int)ans);
-        }
-        return l;
-    }
+    // public List<Integer> generaterow(int i){
+    //     long ans=1;
+    //     List<Integer>l=new ArrayList<>();
+    //     l.add(1);
+    //     for(int j=1;j<i;j++){
+    //         ans*=(i-j);
+    //         ans/=j;
+    //         l.add((int)ans);
+    //     }
+    //     return l;
+    // }
 }
