60347ecfe00dac4549c56da7206bb44c3a2c9be9
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-20 09:06:45 +0530

    Time: 22 ms (80.60%) | Memory: 66.1 MB (55.72%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -1,21 +1,21 @@
-class pair{
-    int row;
-    int col;
-    pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
+// class pair{
+//     int row;
+//     int col;
+//     pair(int row,int col){
+//         this.row=row;
+//         this.col=col;
+//     }
+// }
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
-        Map<Integer,pair>mp=new HashMap<>();
+        Map<Integer,Integer>mp=new HashMap<>();
         int []rc=new int[n];
         int []cc=new int[m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                mp.put(mat[i][j],new pair(i,j));
+                mp.put(mat[i][j],i*m+j);
                 rc[i]++;
                 cc[j]++;
             }
@@ -23,9 +23,9 @@ class Solution {
         int k=0;
 
         for(int i:arr){
-            pair idx=mp.get(i);
-            int row=idx.row;
-            int col=idx.col;
+            int idx=mp.get(i);
+            int row=idx/m;
+            int col=idx%m;
             rc[row]--;
             cc[col]--;
             if(rc[row]==0 || cc[col]==0) return k;
