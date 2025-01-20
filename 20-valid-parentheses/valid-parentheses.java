e76884e1c6b3dbbca1133d6e04769c629963f07d
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-20 10:35:31 +0530

    Time: 3 ms (42.19%) | Memory: 41.6 MB (89.35%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,27 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character>st=new Stack<>();
-
-        for(int i=0;i<s.length();i++){
-            if(!st.isEmpty()){
-                if(st.peek()=='(' && s.charAt(i)==')'){
-                    st.pop();
-                }
-                else if(st.peek()=='{' && s.charAt(i)=='}'){
-                    st.pop();
-                }
-                else if(st.peek()=='[' && s.charAt(i)==']'){
-                    st.pop();
-                }
-                else{
-                    st.push(s.charAt(i));
-                }
+        for(char ch:s.toCharArray()){
+            if(!st.isEmpty() && (ch=='}' ||ch==')' ||ch==']')){
+                if(st.peek()=='{' && ch=='}') st.pop();
+                else if(st.peek()=='[' && ch==']') st.pop();
+                else if(st.peek()=='(' && ch==')') st.pop();
+                else return false;
             }
-            else{
-                st.push(s.charAt(i));
-            }
-        }
-
+            else st.push(ch);
+        } 
         return st.isEmpty();
     }
 }
