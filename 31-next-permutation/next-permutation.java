04c158e0b89bb3c1bd383c68ce7535a41072b534
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-22 20:25:59 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (71.10%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,26 +1,23 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i=nums.length-2;
+        int  i=nums.length-2;
         while(i>=0 && nums[i+1]<=nums[i]) i--;
-
         if(i>=0){
             int j=nums.length-1;
-            while(nums[j]<=nums[i]){
-                j--;
-            }
-            swap(nums,i,j);
+            while(j>=0 && nums[i]>=nums[j]) j--;
+            swap(i,j,nums);
         }
-        reverse(nums,i+1);
+        rev(nums,i+1);
     }
-    public void reverse(int[]nums,int i){
+    public void rev(int []nums,int i){
         int j=nums.length-1;
         while(i<j){
-            swap(nums,i++,j--);
+            swap(i++,j--,nums);
         }
     }
-    public void swap(int []nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    public void swap(int i,int j,int[]nums){
+        int r=nums[j];
+        nums[j]=nums[i];
+        nums[i]=r;
     }
 }
