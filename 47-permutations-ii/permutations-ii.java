125936b7295031b1c5c67034e1ad59badab39086
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-22 21:09:38 +0530

    Time: 1 ms (99.81%) | Memory: 45.1 MB (35.40%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -2,24 +2,24 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>>ll=new ArrayList<>();
         List<Integer>l=new ArrayList<>();
-        boolean[]used=new boolean[nums.length];
         Arrays.sort(nums);
-        print(nums,l,ll,used);
+        boolean[]used=new boolean[nums.length];
+        call(ll,l,used,nums);
         return ll;
     }
-    public void print(int[]nums,List<Integer>l,List<List<Integer>>ll,boolean[]used){
+    public void call(List<List<Integer>>ll,List<Integer>l,boolean[]used,int[]nums){
         if(l.size()==nums.length){
             ll.add(new ArrayList<>(l));
             return;
         }
         for(int i=0;i<nums.length;i++){
             if(used[i]) continue;
-            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+            if(i+1<nums.length && nums[i]==nums[i+1] &&!used[i+1]) continue;
             used[i]=true;
             l.add(nums[i]);
-            print(nums,l,ll,used);
+            call(ll,l,used,nums);
+            l.remove(l.size()-1);
             used[i]=false;
-            l.remove(l.size()-1);            
         }
     }
 }
