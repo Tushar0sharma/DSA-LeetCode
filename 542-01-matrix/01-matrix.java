1b9aad322fff293583c104934fcaf4504501ee39
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-22 11:27:27 +0530

    Time: 13 ms (84.74%) | Memory: 46.6 MB (97.72%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,31 +1,27 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+        Queue<int[]>q=new LinkedList<>();
         int n=mat.length;
         int m=mat[0].length;
-        boolean [][]vis=new boolean[n][m];
-        Queue<int[]>q=new LinkedList<>();
+        int [][]vis=new int[n][m];
+        for(int []i:vis) Arrays.fill(i,Integer.MAX_VALUE);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
-                    vis[i][j]=true;
-                    q.add(new int[]{i,j,0});
-                }
+                if(mat[i][j]==0){vis[i][j]=0; q.add(new int[]{i,j,0});}
             }
         }
-        int []r={0,0,-1,1};
-        int []c={1,-1,0,0};
+        int [][]dir={{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.isEmpty()){
             int []p=q.poll();
-            mat[p[0]][p[1]]=p[2];
-            for(int i=0;i<4;i++){
-                int nr=p[0]+r[i];
-                int nc=p[1]+c[i];
-                if(nr>=0 && nc>=0 && nr<n && nc<m && !vis[nr][nc]){
-                    vis[nr][nc]=true;
+            for(int []i:dir){
+                int nr=i[0]+p[0];
+                int nc=i[1]+p[1];
+                if(nr>=0 &&nc<m &&nr<n &&nc>=0 &&vis[nr][nc]>1+p[2]){
+                    vis[nr][nc]=p[2]+1;
                     q.add(new int[]{nr,nc,p[2]+1});
                 }
             }
         }
-        return mat;
+        return vis;
     }
 }
