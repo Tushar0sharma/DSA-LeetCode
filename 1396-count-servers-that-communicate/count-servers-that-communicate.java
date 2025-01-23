13d0af97787af47817c5e8e343232b9c492a348b
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-23 08:23:16 +0530

    Time: 2 ms (100.00%) | Memory: 52.7 MB (27.79%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -2,46 +2,22 @@ class Solution {
     public int countServers(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int []par=new int[n+m+1];
-        int []size=new int[n+m+1];
-        for(int i=0;i<n+m+1;i++){
-            par[i]=i;
-        }
+        int []row=new int[n];
+        int []col=new int[m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    int row=i;
-                    int col=j+n;
-                    union(row,col,par,size);
+                    row[i]++;
+                    col[j]++;
                 }
             }
         }
-        System.out.println(Arrays.toString(size));
-        int cnt=0;
-        for(int i=0;i<n+m+1;i++){
-            if(size[i]>1 && par[i]==i) cnt+=size[i];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((row[i]>1 || col[j]>1) && grid[i][j]==1) ans++;
+            }
         }
-        return cnt;
-    }
-    public int find(int p,int []par){
-        if(p==par[p]) return p;
-        return par[p]=find(par[p],par);
-    }
-    public void union(int u,int v,int []par,int []size){
-        int pu=find(u,par);
-        int pv=find(v,par);
-        if(pu==pv){
-            size[pu]++;
-            par[pv]=pu;
-            return;
-        }
-        if(size[pu]>size[pv]){
-            size[pu]+=size[pv]+1;
-            par[pv]=pu;
-        }
-        else{
-            size[pv]+=size[pu]+1;
-            par[pu]=pv;
-        }
+        return ans;        
     }
 }
