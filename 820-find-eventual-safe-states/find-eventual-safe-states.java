caf07ee384599933900de8254e25d01b7adaae1e
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-24 14:30:30 +0530

    Time: 4 ms (99.02%) | Memory: 55.5 MB (53.71%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,32 +1,26 @@
 class Solution {
+    enum state{
+        safe,
+        unsafe
+    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer>l=new ArrayList<>();
         int n=graph.length;
-        int []in=new int[n];
-        Map<Integer,List<Integer>>mp=new HashMap<>();
-        for(int i=0;i<n;i++) mp.put(i,new ArrayList<>());
-        int []out=new int[n];
-        for(int i=0;i<n;i++){
-            for(int j:graph[i]){
-                out[i]++;
-                in[j]++;
-                mp.get(j).add(i);
+        state[]s=new state[n];
+        for(int node=0;node<n;node++){
+            if(issafe(graph,node,s)){
+                l.add(node);
             }
         }
-        // System.out.println(Arrays.toString(out));
-        System.out.println(mp);
-        
-        Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<n;i++) if(out[i]==0) q.add(i);
-        // System.out.println(q);
-        while(!q.isEmpty()){
-            int p=q.poll();
-            l.add(p);
-            for(int i:mp.get(p)){
-                if(--out[i]==0) q.add(i);
-            }
-        }
-        Collections.sort(l);
         return l;
+    }
+    public boolean issafe(int[][]graph,int node,state[]s){
+        if(s[node]!=null) return s[node]==state.safe;
+        s[node]=state.unsafe;
+        for(int i:graph[node]){
+            if(!issafe(graph,i,s)) return false;
+        }
+        s[node]=state.safe;
+        return true;
     }
 }
