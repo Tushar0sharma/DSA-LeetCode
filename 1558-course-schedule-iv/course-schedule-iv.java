2bb7cbdf451eb6a7894e4c53d87dcd7f19aac66c
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-27 10:13:04 +0530

    Time: 41 ms (56.32%) | Memory: 48.3 MB (51.62%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 for(int k=0;k<n;k++){
-                    reach[j][k]=reach[j][k]||(reach[j][i]&&reach[i][k]);
+                    reach[j][k]=reach[j][k]||reach[j][i]&&reach[i][k];
                 }
             }
         }
