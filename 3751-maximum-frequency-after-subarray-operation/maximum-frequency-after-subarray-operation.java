6ccfdb1c9cd662ef085709786d6e9b0770f1d433
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-27 21:02:52 +0530

    Time: 67 ms (100.00%) | Memory: 61.3 MB (100.00%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
@@ -5,11 +5,11 @@ class Solution {
         int max=0;
         int ans=0;
         for(int i=1;i<=50;i++){
-            if(i==k) continue;
+            // if(i==k) continue;
             int sum=0;
             for(int j:nums){
                 if(j==i) sum++;
-                else if(j==k) sum--;
+                 if(j==k) sum--;
                 if(sum<0) sum=0;
                 max=Math.max(max,sum);
             }
