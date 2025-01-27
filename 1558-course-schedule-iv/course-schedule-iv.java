2b93767e73ad05ca1427dc9beeecb0dfc5cc5ae8
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-27 10:12:47 +0530

    Time: 41 ms (56.32%) | Memory: 48.1 MB (64.27%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,35 +1,20 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries) {
         List<Boolean>l=new ArrayList<>();
-        Map<Integer,List<Integer>>mp=new HashMap<>();
-        for(int i=0;i<n;i++) mp.put(i,new ArrayList<>());
         boolean [][]reach=new boolean[n][n];
-        for(int[]i:prerequisites){
-            mp.get(i[0]).add(i[1]);
+        for(int []p:prerequisites){
+            reach[p[0]][p[1]]=true;
         }
-        // System.out.println(mp);
-        Queue<Integer>q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            bfs(reach,i,q,mp);
-        }
-        for(int[]i:queries){
-            l.add(reach[i[0]][i[1]]);
-        }
-        return l;
-    }
-    public void bfs(boolean[][]reach,int src,Queue<Integer>q,Map<Integer,List<Integer>>mp){
-        q.add(src);
-        boolean []vis=new boolean[reach.length];
-        vis[src]=true;
-        while(!q.isEmpty()){
-            int p=q.poll();
-            reach[src][p]=true;
-            for(int i:mp.get(p)){
-                if(!vis[i]){    
-                    vis[i]=true;
-                    q.add(i);
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    reach[j][k]=reach[j][k]||(reach[j][i]&&reach[i][k]);
                 }
             }
         }
+        for(int []i:queries){
+            l.add(reach[i[0]][i[1]]);
+        }
+        return l;        
     }
 }
