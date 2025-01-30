39453c245d3a39e24ebcec6ace3441c8e1379104
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-01-30 11:09:52 +0530

    Time: 8 ms (16.72%) | Memory: 45.3 MB (91.98%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -2,20 +2,25 @@ class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
         int n=nums1.length;
         int m=nums2.length;
+        Map<Integer,Integer>mp1=new HashMap<>();
+        Map<Integer,Integer>mp2=new HashMap<>();
+        
+        for(int i=0;i<n;i++){
+            mp1.put(nums1[i],mp1.getOrDefault(nums1[i],0)+1);
+        }
+        for(int i=0;i<m;i++){
+            mp2.put(nums2[i],mp2.getOrDefault(nums2[i],0)+1);
+        }
         int ans1=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(nums1[i]==nums2[j]){
-                    ans1++;break;
-                }
+            if(mp2.containsKey(nums1[i])){
+                ans1++;
             }
         }
         int ans2=0;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(nums1[j]==nums2[i]){
-                    ans2++;break;
-                }
+            if(mp1.containsKey(nums2[i])){
+                ans2++;
             }
         }
         return new int[]{ans1,ans2};        
