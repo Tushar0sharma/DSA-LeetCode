d27fb7add35d9948c327a763416a80887ad8795b
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-02-03 09:14:49 +0530

    Time: 1 ms (93.94%) | Memory: 43 MB (22.90%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,27 +1,21 @@
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        int in=1;
-        int de=1;
-        int inmax=0;
-        int demax=0;
-        if(nums.length==1) return 1;
-
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]<nums[i+1]){
-                in++;
-            }
-            else{
-                in=1;
-            }
-            if(nums[i]>nums[i+1]){
-                de++;
-            }
-            else{
-                de=1;
-            }
-            inmax=Math.max(inmax,in);
-            demax=Math.max(de,demax);
+        int n=nums.length;
+        if( nums.length==1) return 1;
+        int []pre=new int[n];
+        Arrays.fill(pre,1);
+        int a=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]) pre[i]+=pre[i-1];
+            a=Math.max(a,pre[i]);
         }
-        return Math.max(inmax,demax);
+        int b=0;
+        int []post=new int[n];
+        Arrays.fill(post,1);
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]) post[i]+=post[i-1];
+            b=Math.max(b,post[i]);
+        }
+        return Math.max(a,b);
     }
 }
