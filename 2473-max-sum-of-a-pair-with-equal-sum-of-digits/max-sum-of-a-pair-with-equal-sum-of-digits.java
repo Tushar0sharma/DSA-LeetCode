872b8c62ec864e342f7e56390d6dff4cde06b5aa
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-02-12 10:30:52 +0530

    Time: 107 ms (15.12%) | Memory: 56.6 MB (71.35%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,6 +1,5 @@
 class Solution {
     public int maximumSum(int[] nums) {
-        Map<Integer,Integer>freqsum=new HashMap<>();
         Map<Integer,List<Integer>>mp=new HashMap<>();
         int max=-1;
         for(int number:nums){
@@ -10,10 +9,7 @@ class Solution {
                 sum+=num%10;
                 num/=10;
             }
-            freqsum.put(sum,freqsum.getOrDefault(sum,0)+1);
-            if(!mp.containsKey(sum)){
-                mp.put(sum,new ArrayList<>());
-            }
+            mp.putIfAbsent(sum, new ArrayList<>());
             mp.get(sum).add(number);
         }
         for (List<Integer> list : mp.values()) {
