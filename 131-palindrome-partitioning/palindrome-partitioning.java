ee85ec4e10b717bb0698620a6b9ddc8eb0e51813
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-02-15 16:50:47 +0530

    Time: 8 ms (57.33%) | Memory: 57.1 MB (43.07%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -2,34 +2,30 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>>ll=new ArrayList<>();
         List<String>l=new ArrayList<>();
-        partition(s,ll,l);
+        int n=s.length();
+        boolean[][]dp=new boolean[n][n];
+        for(int len=1;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                if(s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=(len<=2)||dp[i+1][j-1];
+                }
+            }
+        }
+        call(ll,l,dp,s,0);
         return ll;
     }
-    public void partition(String s,List<List<String>>ll,List<String>l){
-        if(s.length()==0){
+    public void call(List<List<String>>ll,List<String>l,boolean[][]dp,String s,int i){
+        if(s.length()==i){
             ll.add(new ArrayList<>(l));
             return;
         }
-        for(int i=1;i<=s.length();i++){
-            String k=s.substring(0,i);
-            if(check(k)){
-                l.add(k);
-                partition(s.substring(i),ll,l);
+        for(int j=i;j<s.length();j++){
+            if(dp[i][j]){
+                l.add(s.substring(i,j+1));
+                call(ll,l,dp,s,j+1);
                 l.remove(l.size()-1);
             }
         }
-
-    }
-    public boolean check(String s1){
-       int i=0;
-       int j=s1.length()-1;
-       while(i<j){
-           if(s1.charAt(i)!=s1.charAt(j)){
-               return false;
-           }
-           i++;
-           j--;
-       }
-       return true;
     }
 }
