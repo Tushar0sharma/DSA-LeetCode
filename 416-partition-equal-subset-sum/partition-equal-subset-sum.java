cde5a28027d1556703c6804844f4281bf9359e3d
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-02-15 15:16:31 +0530

    Time: 22 ms (89.75%) | Memory: 57.4 MB (24.57%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,23 +2,14 @@ class Solution {
     public boolean canPartition(int[] nums) {
         int sum=0;
         for(int i:nums) sum+=i;
-        int reqSum=sum/2;
-        int n=nums.length;
         if(sum%2!=0) return false;
-        boolean[]dp=new boolean [reqSum+1];
-        dp[0]=true;
-        if(nums[0]<=reqSum) dp[nums[0]]=true;
-        for(int i=1;i<n;i++){
-            boolean []dp1=new boolean [reqSum+1];
-            dp1[0]=true;
-            for(int j=1;j<=reqSum;j++){
-                boolean nottake=dp[j];
-                boolean take=false;
-                if(nums[i]<=j) take=dp[j-nums[i]];
-                dp1[j]=take||nottake;
-            }
-            dp=dp1;
-        }
-        return dp[reqSum];
+        Boolean [][]dp=new Boolean[nums.length][sum/2];
+        return call(sum/2,nums,0,0,dp);
+    }
+    public boolean call(int k,int []nums,int i,int sum,Boolean [][]dp){
+        if(sum==k) return true;
+        if(i==nums.length || sum>k) return false;
+        if(dp[i][sum]!=null) return dp[i][sum];
+        return dp[i][sum]=call(k,nums,i+1,sum+nums[i],dp)||call(k,nums,i+1,sum,dp);
     }
 }
