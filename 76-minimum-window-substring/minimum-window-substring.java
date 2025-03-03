8d0cdb1b34373f2c4a6d18d893bc51d51c1f7b0d
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-03-03 11:02:58 +0530

    Time: 119 ms (15.14%) | Memory: 46.1 MB (5.31%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,33 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character,Integer>mp=new HashMap<>();
-        for(char ch:t.toCharArray()){
-            mp.put(ch,mp.getOrDefault(ch,0)+1);
-        }
-        int unique=mp.size();
-        int j=0;
         Map<Character,Integer>mp1=new HashMap<>();
+        Map<Character,Integer>mp2=new HashMap<>();
+        for(char ch:t.toCharArray()){
+            mp2.put(ch,mp2.getOrDefault(ch,0)+1);
+        }
+        String ans="";
+        int j=0;
+        int unique=mp2.size();
         int make=0;
         int min=Integer.MAX_VALUE;
-        String ans="";
         for(int i=0;i<s.length();i++){
             mp1.put(s.charAt(i),mp1.getOrDefault(s.charAt(i),0)+1);
-            if(mp.containsKey(s.charAt(i)) && mp1.get(s.charAt(i)).equals(mp.get(s.charAt(i)))){
+            if(mp2.containsKey(s.charAt(i)) && mp1.get(s.charAt(i)).equals(mp2.get(s.charAt(i)))){
                 make++;
             }
             while(make==unique){
                 int len=i-j;
                 if(min>len){
-                    ans=s.substring(j,i+1);
                     min=len;
+                    ans=s.substring(j,i+1);
                 }
-                mp1.put(s.charAt(j),mp1.getOrDefault(s.charAt(j),0)-1);
-                if(mp.containsKey(s.charAt(j)) && mp.get(s.charAt(j))>mp1.get(s.charAt(j))){
+                mp1.put(s.charAt(j),mp1.get(s.charAt(j))-1);
+                if(mp2.containsKey(s.charAt(j)) && mp2.get(s.charAt(j))>mp1.get(s.charAt(j))){
                     make--;
                 }
                 j++;
             }
-        }   
+        }
         return ans;
     }
 }
