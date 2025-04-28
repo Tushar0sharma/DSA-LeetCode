38dce4f0ed4a1cab96ddee09bd24f62ced683ad3
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-04-28 14:55:39 +0530

    Time: 3 ms (13.68%) | Memory: 42.5 MB (62.24%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,49 +1,35 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<int[]>q=new LinkedList<>();
         int n=grid.length;
         int m=grid[0].length;
-        int [][]dp=new int[n][m];
-        for(int []i:dp) Arrays.fill(i,Integer.MAX_VALUE);
-        Queue<int[]>q=new LinkedList<>();
+        int a=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.add(new int[]{i,j,0});
-                }
-                if(grid[i][j]==0){
-                    dp[i][j]=0;
-                }
+                if(grid[i][j]==2) q.add(new int[]{i,j});
+                else if(grid[i][j]==1) a++;
             }
         }
+        if(a==0) return 0;
+        int [][]dir={{0,1},{-1,0},{1,0},{0,-1}};
+        int ans=0;
         while(!q.isEmpty()){
-            int []p=q.poll();
-            dp[p[0]][p[1]]=Math.min(dp[p[0]][p[1]],p[2]);
-            int i=p[0];
-            int j=p[1];
-            if(i+1>=0 && j>=0 && i+1<n && j<m && grid[i+1][j]==1){
-                grid[i+1][j]=2;
-                q.add(new int[]{i+1,j,p[2]+1});
-            }
-            if(i-1>=0 && j>=0 && i-1<n && j<m && grid[i-1][j]==1){
-                grid[i-1][j]=2;
-                q.add(new int[]{i-1,j,p[2]+1});
-            }
-            if(i>=0 && j-1>=0 && i<n && j-1<m && grid[i][j-1]==1){
-                grid[i][j-1]=2;
-                q.add(new int[]{i,j-1,p[2]+1});
-            }
-            if(i>=0 && j+1>=0 && i<n && j+1<m && grid[i][j+1]==1){
-                grid[i][j+1]=2;
-                q.add(new int[]{i,j+1,p[2]+1});
+            int s=q.size();
+            ans++;
+            for(int k=0;k<s;k++){
+                int []p=q.poll();
+                for(int[]i:dir){
+                    int r=i[0]+p[0];
+                    int c=i[1]+p[1];
+                    if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==1){
+                        grid[r][c]=2;
+                        a--;
+                        q.add(new int[]{r,c});
+                    }
+                }
             }
         }
-        int max=0;
-        for(int []i:dp){
-            for(int j:i){
-                if(j==Integer.MAX_VALUE) return -1;
-                max=Math.max(max,j);
-            } 
-        }    
-        return max;    
+        if(a!=0) return -1;
+        return ans-1;
     }
 }
